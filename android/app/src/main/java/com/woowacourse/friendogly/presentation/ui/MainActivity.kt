5dbc09893b4b775e299b75cb36a1ab5550159f98
JunJangE <wnswkd486@gmail.com>
2024-07-10 21:22:23 +0900

    chore: 패키지 분리

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/MainActivity.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.woowacourse.friendogly
+package com.woowacourse.friendogly.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.woowacourse.friendogly.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
