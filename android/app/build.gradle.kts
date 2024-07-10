fbcc94e4c27febcd28717b64a7f362757e901926
JunJangE <wnswkd486@gmail.com>
2024-07-10 21:51:48 +0900

    chore: gradle test 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
+    testImplementation(libs.bundles.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
