23c485ccf26e005ab7c295546871aecb9db0037f
JunJangE <wnswkd486@gmail.com>
2024-07-11 18:19:16 +0900

    test: minSdk 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.woowacourse.friendogly"
-        minSdk = 24
+        minSdk = 25
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
