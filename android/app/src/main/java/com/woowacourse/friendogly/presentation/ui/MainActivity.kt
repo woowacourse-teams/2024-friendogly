d01afcd551816397ea2d2712762abd2bdd839f2e
JunJangE <wnswkd486@gmail.com>
2024-07-13 01:23:52 +0900

    feat: 바텀 내비게이션

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/MainActivity.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.woowacourse.friendogly.presentation.ui
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.woowacourse.friendogly.R
+import com.woowacourse.friendogly.databinding.ActivityMainBinding
+import com.woowacourse.friendogly.presentation.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
+    override fun initCreateView() {
+        initNavController()
+    }
+
+    private fun initNavController() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment, R.id.woofFragment, R.id.chatFragment, R.id.myPageFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
         }
+        binding.bottomNavi.setupWithNavController(navController)
+        binding.bottomNavi.setOnItemReselectedListener {}
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNavi.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNavi.visibility = View.GONE
     }
 }
