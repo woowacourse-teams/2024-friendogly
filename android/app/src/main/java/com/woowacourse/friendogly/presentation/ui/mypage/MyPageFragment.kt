aa1268e5b114cbba2edc58a113a98c48a0534e2b
JunJangE <wnswkd486@gmail.com>
2024-07-13 01:16:58 +0900

    feat: my page fragment 생성

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/mypage/MyPageFragment.kt
@@ -0,0 +1,9 @@
+package com.woowacourse.friendogly.presentation.ui.mypage
+
+import com.woowacourse.friendogly.R
+import com.woowacourse.friendogly.databinding.FragmentMyPageBinding
+import com.woowacourse.friendogly.presentation.base.BaseFragment
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    override fun initViewCreated() {}
+}
