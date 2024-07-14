ab229e8b826e71e1f4e13358ff28cd03921d1a85
김진우 <jinwoo3661@naver.com>
2024-07-14 23:56:48 +0900

    feat: viewModel init후 연결

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/detail/GroupDetailFragment.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/detail/GroupDetailFragment.kt
@@ -1,11 +1,14 @@
 package com.woowacourse.friendogly.presentation.ui.grouplist.detail
 
+import androidx.fragment.app.viewModels
 import com.woowacourse.friendogly.R
 import com.woowacourse.friendogly.databinding.FragmentGroupDetailBinding
 import com.woowacourse.friendogly.presentation.base.BaseFragment
 
 class GroupDetailFragment :
     BaseFragment<FragmentGroupDetailBinding>(R.layout.fragment_group_detail) {
-    override fun initViewCreated() {}
-
+    private val viewModel: GroupDetailViewModel by viewModels()
+    override fun initViewCreated() {
+        binding.vm = viewModel
+    }
 }
---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/detail/GroupDetailViewModel.kt
@@ -0,0 +1,5 @@
+package com.woowacourse.friendogly.presentation.ui.grouplist.detail
+
+import com.woowacourse.friendogly.presentation.base.BaseViewModel
+
+class GroupDetailViewModel: BaseViewModel() {}
