8a85e87472a5d3d780c96cd554cca575514feaba
김진우 <jinwoo3661@naver.com>
2024-07-14 19:49:25 +0900

    feat: 멍개 리스트 기능 구현

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/GroupListFragment.kt
@@ -0,0 +1,12 @@
+package com.woowacourse.friendogly.presentation.ui.grouplist
+
+import com.woowacourse.friendogly.R
+import com.woowacourse.friendogly.databinding.FragmentGroupListBinding
+import com.woowacourse.friendogly.presentation.base.BaseFragment
+
+class GroupListFragment() : BaseFragment<FragmentGroupListBinding>(R.layout.fragment_group_list) {
+    override fun initViewCreated() {
+
+    }
+
+}
