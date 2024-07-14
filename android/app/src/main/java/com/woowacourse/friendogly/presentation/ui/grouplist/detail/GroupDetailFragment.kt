2f277122896b4731cd5488c75985872696a05e6b
김진우 <jinwoo3661@naver.com>
2024-07-14 23:47:28 +0900

    feat: 멍개 디테일 ui 구현

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/detail/GroupDetailFragment.kt
@@ -0,0 +1,11 @@
+package com.woowacourse.friendogly.presentation.ui.grouplist.detail
+
+import com.woowacourse.friendogly.R
+import com.woowacourse.friendogly.databinding.FragmentGroupDetailBinding
+import com.woowacourse.friendogly.presentation.base.BaseFragment
+
+class GroupDetailFragment :
+    BaseFragment<FragmentGroupDetailBinding>(R.layout.fragment_group_detail) {
+    override fun initViewCreated() {}
+
+}
