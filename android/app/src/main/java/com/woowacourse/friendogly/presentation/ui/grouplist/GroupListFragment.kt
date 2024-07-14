3f79b6ac02a4b907256c5bba81df7f00c395de26
김진우 <jinwoo3661@naver.com>
2024-07-14 17:13:25 +0900

    style: 린트 체크

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/GroupListFragment.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/GroupListFragment.kt
@@ -6,7 +6,5 @@ import com.woowacourse.friendogly.presentation.base.BaseFragment
 
 class GroupListFragment() : BaseFragment<FragmentGroupListBinding>(R.layout.fragment_group_list) {
     override fun initViewCreated() {
-
     }
-
 }
