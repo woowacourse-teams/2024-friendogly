461e54a1ef06adccdf8ff805f244736ad6031b5d
김진우 <jinwoo3661@naver.com>
2024-07-14 15:17:02 +0900

    feat: 필터 관련 ui model 구현

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/grouplistUiModel/groupfilter/GroupFilter.kt
@@ -0,0 +1,29 @@
+package com.woowacourse.friendogly.presentation.ui.grouplist.grouplistUiModel.groupfilter
+
+sealed class GroupFilter(val filterName: String) {
+    class GenderFilter(filterName: String) : GroupFilter(filterName) {
+        companion object {
+            private fun from(): List<GroupFilter> {
+                return listOf(
+                    GenderFilter("수컷"),
+                    GenderFilter("암컷"),
+                    GenderFilter("중성화 수컷"),
+                    GenderFilter("중성화 암컷"),
+                )
+            }
+        }
+    }
+
+    class SizeFilter(filterName: String) : GroupFilter(filterName) {
+        companion object {
+            private fun from(): List<GroupFilter> {
+                return listOf(
+                    SizeFilter("수컷"),
+                    SizeFilter("암컷"),
+                    SizeFilter("중성화 수컷"),
+                    SizeFilter("중성화 암컷"),
+                )
+            }
+        }
+    }
+}
