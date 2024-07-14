5dbda632a2e7de6ae19caecef7bd58a1bc75c6b0
김진우 <jinwoo3661@naver.com>
2024-07-14 16:51:57 +0900

    feat: group list ui 구현

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/GroupListFragment.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/grouplist/GroupListFragment.kt
@@ -1,11 +1,9 @@
 package com.woowacourse.friendogly.presentation.ui.grouplist
 
-import android.annotation.SuppressLint
 import com.woowacourse.friendogly.R
 import com.woowacourse.friendogly.databinding.FragmentGroupListBinding
 import com.woowacourse.friendogly.presentation.base.BaseFragment
 
-@SuppressLint("ParcelCreator")
 class GroupListFragment() : BaseFragment<FragmentGroupListBinding>(R.layout.fragment_group_list) {
     override fun initViewCreated() {
 
