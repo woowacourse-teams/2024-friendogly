46ea712708b7d0ea26606967667c4761ba63f1b6
김진우 <jinwoo3661@naver.com>
2024-07-15 00:10:25 +0900

    style: 코드 포맷 수정 & 린트 체크

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/list/GroupListFragment.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/list/GroupListFragment.kt
@@ -6,7 +6,8 @@ import com.woowacourse.friendogly.databinding.FragmentGroupListBinding
 import com.woowacourse.friendogly.presentation.base.BaseFragment
 
 class GroupListFragment() : BaseFragment<FragmentGroupListBinding>(R.layout.fragment_group_list) {
-    private val viewModel : GroupListViewModel by viewModels()
+    private val viewModel: GroupListViewModel by viewModels()
+
     override fun initViewCreated() {
         binding.vm = viewModel
     }
