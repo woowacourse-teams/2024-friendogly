5ae8eef5b9680c87425b0d95b691ea8f1ddba58c
ehtjsv2 <ehtjsv2@naver.com>
2024-07-15 21:26:34 +0900

    fix: pet 생성할 때 member id null로 저장되는 버그 수정

--- backend/src/main/java/com/woowacourse/friendogly/pet/service/PetCommandService.java
+++ backend/src/main/java/com/woowacourse/friendogly/pet/service/PetCommandService.java
@@ -1,5 +1,7 @@
 package com.woowacourse.friendogly.pet.service;
 
+import com.woowacourse.friendogly.member.domain.Member;
+import com.woowacourse.friendogly.member.repository.MemberRepository;
 import com.woowacourse.friendogly.pet.domain.Pet;
 import com.woowacourse.friendogly.pet.dto.request.SavePetRequest;
 import com.woowacourse.friendogly.pet.repository.PetRepository;
@@ -9,13 +11,17 @@ import org.springframework.stereotype.Service;
 public class PetCommandService {
 
     private final PetRepository petRepository;
+    private final MemberRepository memberRepository;
 
-    public PetCommandService(PetRepository petRepository) {
+    public PetCommandService(PetRepository petRepository, MemberRepository memberRepository) {
         this.petRepository = petRepository;
+        this.memberRepository = memberRepository;
     }
 
     public Long savePet(SavePetRequest savePetRequest) {
-        Pet pet = savePetRequest.toEntity();
+        Member member = memberRepository.findById(savePetRequest.memberId())
+                .orElseThrow(()->new IllegalArgumentException("존재하지 않는 Member Id 입니다."));
+        Pet pet = savePetRequest.toEntity(member);
         Pet savedPet = petRepository.save(pet);
         return savedPet.getId();
     }
