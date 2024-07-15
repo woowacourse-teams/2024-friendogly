1d1bc20a57e39f2c520016950a422c94c026fad7
김도선 <ehtjsv2@naver.com>
2024-07-15 15:39:16 +0900

    스프링 프로젝트 초기 구성 (#52)

---
+++ backend/src/test/java/com/woowacourse/friendogly/FriendoglyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.friendogly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendoglyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
