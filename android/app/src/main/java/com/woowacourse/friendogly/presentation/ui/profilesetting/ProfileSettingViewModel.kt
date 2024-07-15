e132c16746ba5f41221d820af74b024919c4d3e2
JunJangE <wnswkd486@gmail.com>
2024-07-15 17:28:33 +0900

    프로필 설정 (#53)

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/profilesetting/ProfileSettingViewModel.kt
@@ -0,0 +1,44 @@
+package com.woowacourse.friendogly.presentation.ui.profilesetting
+
+import android.graphics.Bitmap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.woowacourse.friendogly.presentation.base.BaseViewModel
+import com.woowacourse.friendogly.presentation.base.Event
+import com.woowacourse.friendogly.presentation.base.emit
+import okhttp3.MultipartBody
+
+class ProfileSettingViewModel : BaseViewModel() {
+    private val _uiState: MutableLiveData<ProfileSettingUiState> =
+        MutableLiveData(ProfileSettingUiState())
+    val uiState: LiveData<ProfileSettingUiState> get() = _uiState
+
+    val nickname = MutableLiveData<String>("")
+
+    private val _navigateAction: MutableLiveData<Event<ProfileSettingNavigationAction>> =
+        MutableLiveData(null)
+    val navigateAction: LiveData<Event<ProfileSettingNavigationAction>> get() = _navigateAction
+
+    fun selectProfileImage() {
+        _navigateAction.emit(ProfileSettingNavigationAction.NavigateToSetProfileImage)
+    }
+
+    fun submitProfileSelection() {
+        _navigateAction.emit(ProfileSettingNavigationAction.NavigateToHome)
+    }
+
+    fun updateProfileImage(bitmap: Bitmap) {
+        val state = _uiState.value ?: return
+        _uiState.value = state.copy(profileImage = bitmap)
+    }
+
+    fun resetProfileImage() {
+        val state = _uiState.value ?: return
+        _uiState.value = state.copy(profileImage = null)
+    }
+
+    fun updateProfileFile(file: MultipartBody.Part) {
+        val state = _uiState.value ?: return
+        _uiState.value = state.copy(profilePath = file)
+    }
+}
