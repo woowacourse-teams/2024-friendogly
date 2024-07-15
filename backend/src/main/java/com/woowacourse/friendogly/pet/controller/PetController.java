ecefd0f3b10b706d23fd7d48ebdbbc73c5c338ed
김도선 <ehtjsv2@naver.com>
2024-07-15 20:39:11 +0900

    feat: 펫 생성 기능 구현 (#54)

--- backend/src/main/java/com/woowacourse/friendogly/pet/controller/PetController.java
+++ backend/src/main/java/com/woowacourse/friendogly/pet/controller/PetController.java
@@ -1,7 +1,31 @@
 package com.woowacourse.friendogly.pet.controller;
 
+import com.woowacourse.friendogly.pet.dto.request.SavePetRequest;
+import com.woowacourse.friendogly.pet.service.PetCommandService;
+import com.woowacourse.friendogly.pet.service.PetQueryService;
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/pet")
 public class PetController {
+
+    private final PetQueryService petQueryService;
+    private final PetCommandService petCommandService;
+
+    public PetController(PetQueryService petQueryService, PetCommandService petCommandService) {
+        this.petQueryService = petQueryService;
+        this.petCommandService = petCommandService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> SavePet(@RequestBody SavePetRequest savePetRequest) {
+        Long petId = petCommandService.savePet(savePetRequest);
+        return ResponseEntity.created(URI.create("/pet/" + petId)).build();
+    }
 }
+
--- backend/src/main/java/com/woowacourse/friendogly/pet/domain/SizeType.java
+++ backend/src/main/java/com/woowacourse/friendogly/pet/domain/SizeType.java
@@ -1,8 +1,23 @@
 package com.woowacourse.friendogly.pet.domain;
 
 public enum SizeType {
-    SMALL,
-    MEDIUM,
-    LARGE,
+    SMALL("small"),
+    MEDIUM("medium"),
+    LARGE("large"),
     ;
+
+    private final String value;
+
+    SizeType(String value) {
+        this.value = value;
+    }
+
+    public static SizeType toSizeType(String sizeType) {
+        for (SizeType value : values()) {
+            if (value.value.equals(sizeType)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 SizeType이 입력되었습니다.");
+    }
 }
