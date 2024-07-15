764ac8a4ba8a6b9bac8a1ce34082d0bbda28bc59
김도선 <ehtjsv2@naver.com>
2024-07-15 20:39:11 +0900

    feat: 펫 생성 기능 구현 (#54)

--- backend/src/main/java/com/woowacourse/friendogly/pet/domain/Gender.java
+++ backend/src/main/java/com/woowacourse/friendogly/pet/domain/Gender.java
@@ -1,7 +1,22 @@
 package com.woowacourse.friendogly.pet.domain;
 
 public enum Gender {
-    MALE,
-    FEMALE,
+    MALE("male"),
+    FEMALE("female"),
     ;
+
+    private final String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+
+    public static Gender toGender(String gender) {
+        for (Gender value : values()) {
+            if (value.value.equals(gender)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 gender가 입력되었습니다.");
+    }
 }
--- backend/src/main/java/com/woowacourse/friendogly/pet/service/PetCommandService.java
+++ backend/src/main/java/com/woowacourse/friendogly/pet/service/PetCommandService.java
@@ -1,8 +1,22 @@
 package com.woowacourse.friendogly.pet.service;
 
+import com.woowacourse.friendogly.pet.domain.Pet;
+import com.woowacourse.friendogly.pet.dto.request.SavePetRequest;
+import com.woowacourse.friendogly.pet.repository.PetRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PetCommandService {
 
+    private final PetRepository petRepository;
+
+    public PetCommandService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    public Long savePet(SavePetRequest savePetRequest) {
+        Pet pet = savePetRequest.toEntity();
+        Pet savedPet = petRepository.save(pet);
+        return savedPet.getId();
+    }
 }
