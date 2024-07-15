634eff8561a5313ccad7b0cd2f03cf59d4cc7294
JunJangE <wnswkd486@gmail.com>
2024-07-15 16:52:46 +0900

    chore: textChangeColor 제거

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/profilesetting/ProfieSettingBindingAdapters.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/profilesetting/ProfieSettingBindingAdapters.kt
@@ -1,37 +1,15 @@
 package com.woowacourse.friendogly.presentation.ui.profilesetting
 
 import android.annotation.SuppressLint
-import android.text.SpannableStringBuilder
-import android.text.Spanned
-import android.text.style.ForegroundColorSpan
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.woowacourse.friendogly.R
 
-fun textChangeColor(
-    text: TextView,
-    color: Int,
-    start: Int,
-    end: Int,
-): SpannableStringBuilder {
-    val builder = SpannableStringBuilder(text.text.toString())
-
-    builder.setSpan(
-        ForegroundColorSpan(color),
-        start,
-        end,
-        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
-    )
-
-    return builder
-}
-
 @SuppressLint("SetTextI18n")
 @BindingAdapter("editTextLength")
 fun TextView.bindEditTextLength(contents: String?) {
     val length = contents?.length ?: 0
-    this.text = "$length/15"
 
     val color =
         if (length != 0) {
@@ -40,8 +18,10 @@ fun TextView.bindEditTextLength(contents: String?) {
             ContextCompat.getColor(context, R.color.gray05)
         }
 
-    this.text = textChangeColor(this, color, 0, length.toString().length)
-    this.setTextColor(color)
+    this.apply {
+        text = "$length/15"
+        setTextColor(color)
+    }
 }
 
 @SuppressLint("UseCompatLoadingForDrawables")
