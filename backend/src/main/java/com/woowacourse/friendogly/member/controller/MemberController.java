9607729af62d40b1fc3004171bbb5989386d6046
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-07-15 20:23:16 +0900

    feat: 회원 생성 기능 추가

--- backend/src/main/java/com/woowacourse/friendogly/member/controller/MemberController.java
+++ backend/src/main/java/com/woowacourse/friendogly/member/controller/MemberController.java
@@ -1,7 +1,28 @@
 package com.woowacourse.friendogly.member.controller;
 
+import com.woowacourse.friendogly.member.dto.request.SaveMemberRequest;
+import com.woowacourse.friendogly.member.dto.response.saveMemberResponse;
+import com.woowacourse.friendogly.member.service.MemberCommandService;
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/member")
 public class MemberController {
+
+    private final MemberCommandService memberCommandService;
+
+    public MemberController(MemberCommandService memberCommandService) {
+        this.memberCommandService = memberCommandService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveMember(@RequestBody SaveMemberRequest request){
+        saveMemberResponse saveMemberResponse = memberCommandService.saveMember(request);
+        return ResponseEntity.created(URI.create("/member/" + saveMemberResponse.id())).build();
+    }
 }
