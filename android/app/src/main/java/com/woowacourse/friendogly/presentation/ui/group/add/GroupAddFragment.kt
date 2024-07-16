6a7027ccebad9d12c2efbcad0896527fb12fd65f
김진우 <jinwoo3661@naver.com>
2024-07-16 18:57:35 +0900

    feat: 데이터바인딩

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/add/GroupAddFragment.kt
@@ -0,0 +1,13 @@
+package com.woowacourse.friendogly.presentation.ui.group.add
+
+import androidx.fragment.app.viewModels
+import com.woowacourse.friendogly.R
+import com.woowacourse.friendogly.databinding.FragmentGroupAddBinding
+import com.woowacourse.friendogly.presentation.base.BaseFragment
+
+class GroupAddFragment : BaseFragment<FragmentGroupAddBinding>(R.layout.fragment_group_add) {
+    private val viewModel: GroupAddViewModel by viewModels()
+    override fun initViewCreated() {
+        binding.vm = viewModel
+    }
+}
---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/add/GroupAddViewModel.kt
@@ -0,0 +1,6 @@
+package com.woowacourse.friendogly.presentation.ui.group.add
+
+import com.woowacourse.friendogly.presentation.base.BaseViewModel
+
+class GroupAddViewModel: BaseViewModel() {}
+
