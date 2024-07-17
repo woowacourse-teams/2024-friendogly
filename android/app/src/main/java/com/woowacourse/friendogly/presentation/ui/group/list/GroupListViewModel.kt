12b17abd5656cc49df494f79e5f1031ddecc7556
김진우 <jinwoo3661@naver.com>
2024-07-17 16:49:09 +0900

    fix: 모델 오류 수정

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/list/GroupListViewModel.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/list/GroupListViewModel.kt
@@ -2,4 +2,6 @@ package com.woowacourse.friendogly.presentation.ui.group.list
 
 import com.woowacourse.friendogly.presentation.base.BaseViewModel
 
-class GroupListViewModel : BaseViewModel()
+class GroupListViewModel : BaseViewModel(){
+
+}
