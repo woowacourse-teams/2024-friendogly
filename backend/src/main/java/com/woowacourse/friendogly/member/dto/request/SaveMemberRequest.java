915f0fda379df9f6b612bcfc653ffcd5e5b1eea7
J-I-H-O <jeongjiho0731@gmail.com>
2024-07-17 15:17:29 +0900

    feat: 이메일 필드 및 검증 어노테이션 추가

--- backend/src/main/java/com/woowacourse/friendogly/member/dto/request/SaveMemberRequest.java
+++ backend/src/main/java/com/woowacourse/friendogly/member/dto/request/SaveMemberRequest.java
@@ -1,7 +1,12 @@
 package com.woowacourse.friendogly.member.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record SaveMemberRequest(@NotBlank String name) {
+public record SaveMemberRequest(
+        @NotBlank @Size(max = 15) String name,
+        @NotBlank @Email String email
+) {
 
 }
