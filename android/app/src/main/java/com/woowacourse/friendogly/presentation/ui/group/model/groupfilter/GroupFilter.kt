ffd79e091b64400974fbece08557111ac1b6b61c
김진우 <jinwoo3661@naver.com>
2024-07-17 20:21:03 +0900

    fix: 충돌 수정

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/model/groupfilter/GroupFilter.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/model/groupfilter/GroupFilter.kt
@@ -22,26 +22,26 @@ sealed class GroupFilter(val filterName: String) {
     companion object {
         fun findGroupFilter(filterName: String): GroupFilter? {
             return when (filterName) {
-                FEMALE_NAME -> GenderFilter.Female()
-                MALE_NAME -> GenderFilter.Male()
-                NEUTRALIZING_FEMALE_NAME -> GenderFilter.NeutralizingFemale()
-                NEUTRALIZING_MALE_NAME -> GenderFilter.NeutralizingMale()
-                SMALL_DOG_NAME -> SizeFilter.SmallDog()
-                MEDIUM_DOG_NAME -> SizeFilter.MediumDog()
-                BIG_DOG_NAME -> SizeFilter.BigDog()
+                FEMALE_NAME -> GenderFilter.Female
+                MALE_NAME -> GenderFilter.Male
+                NEUTRALIZING_FEMALE_NAME -> GenderFilter.NeutralizingFemale
+                NEUTRALIZING_MALE_NAME -> GenderFilter.NeutralizingMale
+                SMALL_DOG_NAME -> SizeFilter.SmallDog
+                MEDIUM_DOG_NAME -> SizeFilter.MediumDog
+                BIG_DOG_NAME -> SizeFilter.BigDog
                 else -> null
             }
         }
 
         fun makeGroupFilterEntry(): List<GroupFilter>{
             return listOf(
-                SizeFilter.SmallDog(),
-                SizeFilter.MediumDog(),
-                SizeFilter.BigDog(),
-                GenderFilter.Male(),
-                GenderFilter.Female(),
-                GenderFilter.NeutralizingMale(),
-                GenderFilter.NeutralizingFemale(),
+                SizeFilter.SmallDog,
+                SizeFilter.MediumDog,
+                SizeFilter.BigDog,
+                GenderFilter.Male,
+                GenderFilter.Female,
+                GenderFilter.NeutralizingMale,
+                GenderFilter.NeutralizingFemale,
             )
         }
 
