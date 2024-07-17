d4e85699a97313829bddd2ebb9b29317527bd884
Gaeun Lee <geun5744@gmail.com>
2024-07-17 17:24:58 +0900

    [AN] feat: 채팅 ui 구현 (#83)

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/chatlist/chat/ChatActivity.kt
@@ -0,0 +1,10 @@
+package com.woowacourse.friendogly.presentation.ui.chatlist.chat
+
+import com.woowacourse.friendogly.R
+import com.woowacourse.friendogly.databinding.ActivityChatBinding
+import com.woowacourse.friendogly.presentation.base.BaseActivity
+
+class ChatActivity : BaseActivity<ActivityChatBinding>(R.layout.activity_chat) {
+    override fun initCreateView() {
+    }
+}
