8faf030917672b0101213eb0f9aa46195b29fa0f
gaeun <geun5744@gmail.com>
2024-07-17 13:41:33 +0900

    feat: chatListFragment 스켈레톤 코드 구현

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/chatlist/ChatListFragment.kt
@@ -0,0 +1,12 @@
+package com.woowacourse.friendogly.presentation.ui.chatlist
+
+import com.woowacourse.friendogly.R
+import com.woowacourse.friendogly.databinding.FragmentChatListBinding
+import com.woowacourse.friendogly.presentation.base.BaseFragment
+
+class ChatListFragment : BaseFragment<FragmentChatListBinding>(R.layout.fragment_chat_list) {
+    override fun initViewCreated() {
+    }
+
+
+}
