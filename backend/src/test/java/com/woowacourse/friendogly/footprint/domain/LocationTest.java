229922477ed54fa0e764c84ac3f62acbd263bb15
takoyakimchi <c4nyou@gmail.com>
2024-07-17 14:18:42 +0900

    refactor: 반경 meter의 기준을 메서드의 파라미터로 넣을 수 있게 변경

--- backend/src/test/java/com/woowacourse/friendogly/footprint/domain/LocationTest.java
+++ backend/src/test/java/com/woowacourse/friendogly/footprint/domain/LocationTest.java
@@ -12,7 +12,7 @@ class LocationTest {
     void isNear_True() {
         Location location1 = new Location(0.0, 0.0);
         Location location2 = new Location(0.0, 0.008993216);
-        assertThat(location1.isNear(location2)).isTrue();
+        assertThat(location1.isWithin(location2, 1000)).isTrue();
     }
 
     @DisplayName("약 1001m 차이 나는 두 위치는 주변에 있지 않다.")
@@ -20,6 +20,6 @@ class LocationTest {
     void isNear_False() {
         Location location1 = new Location(0.0, 0.0);
         Location location2 = new Location(0.0, 0.009001209);
-        assertThat(location1.isNear(location2)).isFalse();
+        assertThat(location1.isWithin(location2, 1000)).isFalse();
     }
 }
