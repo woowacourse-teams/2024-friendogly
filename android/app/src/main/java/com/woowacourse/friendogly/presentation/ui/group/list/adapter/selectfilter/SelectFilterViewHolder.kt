91f0f3ee64280a5220e374285c4a2b3fa5dbbcd9
김진우 <jinwoo3661@naver.com>
2024-07-17 22:45:19 +0900

    feat: select filter Adapter 구현

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/list/adapter/selectfilter/SelectFilterViewHolder.kt
@@ -0,0 +1,20 @@
+package com.woowacourse.friendogly.presentation.ui.group.list.adapter.selectfilter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.woowacourse.friendogly.databinding.ItemFilterBinding
+import com.woowacourse.friendogly.databinding.ItemSelectFilterBinding
+import com.woowacourse.friendogly.presentation.ui.group.list.GroupListActionHandler
+import com.woowacourse.friendogly.presentation.ui.group.list.model.groupfilter.GroupFilter
+
+class SelectFilterViewHolder(
+    private val binding: ItemSelectFilterBinding,
+): RecyclerView.ViewHolder(binding.root){
+    fun bind(
+        filter: GroupFilter,
+        actionHandler : GroupListActionHandler,
+    ){
+        binding.groupFilter = filter
+        binding.actionHandler = actionHandler
+        binding.executePendingBindings()
+    }
+}
