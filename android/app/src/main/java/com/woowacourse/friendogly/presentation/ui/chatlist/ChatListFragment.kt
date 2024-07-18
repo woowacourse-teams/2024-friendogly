0fa7fa83c33df60fab8213b1b692b52d92c8b97d
gaeun <geun5744@gmail.com>
2024-07-18 16:40:34 +0900

    feat: ChatListAdapter 구현

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/chatlist/ChatListFragment.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/chatlist/ChatListFragment.kt
@@ -1,10 +1,21 @@
 package com.woowacourse.friendogly.presentation.ui.chatlist
 
+import androidx.fragment.app.viewModels
 import com.woowacourse.friendogly.R
 import com.woowacourse.friendogly.databinding.FragmentChatListBinding
 import com.woowacourse.friendogly.presentation.base.BaseFragment
+import com.woowacourse.friendogly.presentation.ui.chatlist.adapter.ChatListAdapter
 
 class ChatListFragment : BaseFragment<FragmentChatListBinding>(R.layout.fragment_chat_list) {
+    private val viewModel: ChatListViewModel by viewModels()
+    private lateinit var adapter: ChatListAdapter
+
     override fun initViewCreated() {
+        adapter = ChatListAdapter()
+        binding.rcvChatList.adapter = adapter
+        viewModel.getChats()
+        viewModel.chats.observe(viewLifecycleOwner) { chats ->
+            adapter.submitList(chats)
+        }
     }
 }
