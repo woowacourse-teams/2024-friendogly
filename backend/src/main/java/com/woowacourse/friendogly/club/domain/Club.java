9f88e9b08355c21a8b1191e9dffa369affbca3f8
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-07-18 18:03:06 +0900

    feat: club 도메인 추가
    - 필터링 값 제외하여 구현

---
+++ backend/src/main/java/com/woowacourse/friendogly/club/domain/Club.java
@@ -0,0 +1,97 @@
+package com.woowacourse.friendogly.club.domain;
+
+import com.woowacourse.friendogly.member.domain.Member;
+import com.woowacourse.friendogly.pet.domain.Pet;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Club {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Title title;
+
+    @Embedded
+    private Content content;
+
+    @Column(nullable = false)
+    private MemberCapacity memberCapacity;
+
+    @OneToOne(optional = false)
+    private Member owner;
+
+    @OneToMany
+    private List<Member> participantMembers = new ArrayList<>();
+
+    @OneToMany
+    private List<Pet> participantPets = new ArrayList<>();
+
+    //TODO: 필터링 조건
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Builder
+    public Club(
+            String title,
+            String content,
+            int memberCapacity,
+            Member owner,
+            String imageUrl,
+            List<Pet> participantPets,
+            Status status
+    ) {
+        this.title = new Title(title);
+        this.content = new Content(content);
+        this.memberCapacity = new MemberCapacity(memberCapacity);
+        this.owner = owner;
+        this.imageUrl = imageUrl;
+        this.participantPets = participantPets;
+        this.status = status;
+        createdAt = LocalDateTime.now();
+        participantMembers.add(owner);
+    }
+
+    public int getNumberOfParticipant() {
+        return participantMembers.size();
+    }
+
+    public void addParticipant(Member member) {
+        participantMembers.add(member);
+    }
+
+    public void removeParticipant(Member member) {
+        participantMembers.remove(member);
+    }
+}
