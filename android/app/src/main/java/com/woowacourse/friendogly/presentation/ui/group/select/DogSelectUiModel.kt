e84afd27cd9c954f68a92a6029677049c3f99865
김진우 <jinwoo3661@naver.com>
2024-07-18 10:40:34 +0900

    refactor: java 의존성 제거

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/select/DogSelectUiModel.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/select/DogSelectUiModel.kt
@@ -1,12 +1,10 @@
 package com.woowacourse.friendogly.presentation.ui.group.select
 
-import java.io.Serializable
-
 data class DogSelectUiModel(
     val id: Long,
     val name: String,
     val profileImage: String,
-) : Serializable {
+) {
     var isSelected = false
         private set
 
