d35164aa92fdf142ee69e50b2c23f23e17255e8c
takoyakimchi <c4nyou@gmail.com>
2024-07-18 13:38:38 +0900

    refactor: Location 도메인에 위도, 경도 검증 로직 추가

--- backend/src/test/java/com/woowacourse/friendogly/footprint/domain/LocationTest.java
+++ backend/src/test/java/com/woowacourse/friendogly/footprint/domain/LocationTest.java
@@ -1,11 +1,43 @@
 package com.woowacourse.friendogly.footprint.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.woowacourse.friendogly.exception.FriendoglyException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LocationTest {
+
+    @DisplayName("생성 성공 - 올바른 위도, 경도 범위")
+    @ParameterizedTest
+    @CsvSource(value = {"90.000, 180.000", "-90.000, -180.000", "0.000, 0.000"})
+    void constructor_Success(double latitude, double longitude) {
+        assertThatCode(() -> new Location(latitude, longitude))
+            .doesNotThrowAnyException();
+    }
+
+    @DisplayName("생성 실패 - 올바르지 않은 위도 범위")
+    @ParameterizedTest
+    @ValueSource(doubles = {-90.001, 90.001})
+    void constructor_Fail_IllegalLatitude(double latitude) {
+        assertThatThrownBy(() -> new Location(latitude, 0.0))
+            .isInstanceOf(FriendoglyException.class)
+            .hasMessage("위도 범위가 올바르지 않습니다.");
+    }
+
+    @DisplayName("생성 실패 - 올바르지 않은 위도 범위")
+    @ParameterizedTest
+    @ValueSource(doubles = {-180.001, 180.001})
+    void constructor_Fail_IllegalLongitude(double longitude) {
+        assertThatThrownBy(() -> new Location(0.0, longitude))
+            .isInstanceOf(FriendoglyException.class)
+            .hasMessage("경도 범위가 올바르지 않습니다.");
+    }
 
     @DisplayName("약 999m 차이 나는 두 위치는 주변에 있다.")
     @Test
