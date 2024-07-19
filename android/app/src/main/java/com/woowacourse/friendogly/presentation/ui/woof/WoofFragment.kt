3e9933377f02f01f70410152f7011f40d88d972a
dpcks0509 <dpcks0509@gmail.com>
2024-07-19 15:57:20 +0900

    refactor: 멍멍 짖기 리뷰 반영

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/woof/WoofFragment.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/woof/WoofFragment.kt
@@ -1,7 +1,5 @@
 package com.woowacourse.friendogly.presentation.ui.woof
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import androidx.fragment.app.viewModels
