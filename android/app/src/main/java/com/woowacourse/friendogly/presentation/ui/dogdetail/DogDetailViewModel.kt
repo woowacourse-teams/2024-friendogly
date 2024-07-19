bd5241c52ee774741dd8bb949be163435f8117cb
JunJangE <wnswkd486@gmail.com>
2024-07-19 16:44:57 +0900

    refactor: 현재 페이지 의미 전달을 위한 코드 분리

--- android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/dogdetail/DogDetailViewModel.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/dogdetail/DogDetailViewModel.kt
@@ -9,7 +9,7 @@ class DogDetailViewModel : BaseViewModel() {
     private val _uiState: MutableLiveData<DogDetailUiState> = MutableLiveData(DogDetailUiState())
     val uiState: LiveData<DogDetailUiState> get() = _uiState
 
-    private val _currentPage: MutableLiveData<Int> = MutableLiveData(Int.MAX_VALUE / 2)
+    private val _currentPage: MutableLiveData<Int> = MutableLiveData(MIDDLE_PAGE)
     val currentPage: LiveData<Int> get() = _currentPage
 
     init {
@@ -19,10 +19,14 @@ class DogDetailViewModel : BaseViewModel() {
     private fun fetchDummy() {
         val state = _uiState.value ?: return
         _uiState.value = state.copy(dogs = dogs)
-        _currentPage.value = Int.MAX_VALUE / 2 - (Int.MAX_VALUE / 2) % dogs.size
+        _currentPage.value = MIDDLE_PAGE - (MIDDLE_PAGE) % dogs.size
     }
 
     fun updateCurrentPage(page: Int) {
         _currentPage.value = page
+    }
+
+    companion object {
+        private const val MIDDLE_PAGE = Int.MAX_VALUE / 2
     }
 }
