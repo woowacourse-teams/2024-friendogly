c4ee99e99fa76569d2def1f97ddc72348170a3dc
JunJangE <wnswkd486@gmail.com>
2024-07-20 20:41:07 +0900

    chore: 불필요한 코드 제거

--- android/app/src/main/java/com/woowacourse/friendogly/data/source/KakaoLoginDataSource.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/data/source/KakaoLoginDataSource.kt
@@ -5,14 +5,4 @@ import com.woowacourse.friendogly.data.model.KakaoAccessTokenDto
 
 interface KakaoLoginDataSource {
     suspend fun login(context: Context): Result<KakaoAccessTokenDto>
-
-    companion object {
-        private var instance: KakaoLoginDataSource? = null
-
-        fun setInstance(dataSource: KakaoLoginDataSource) {
-            instance = dataSource
-        }
-
-        fun getInstance(): KakaoLoginDataSource = requireNotNull(instance)
-    }
 }
