7f1827284a1cce9adf0416fa5592d533cd0f571e
J-I-H-O <jeongjiho0731@gmail.com>
2024-07-20 11:46:37 +0900

    refactor: 값 객체 포장

--- backend/src/main/java/com/woowacourse/friendogly/member/domain/Member.java
+++ backend/src/main/java/com/woowacourse/friendogly/member/domain/Member.java
@@ -1,52 +1,33 @@
 package com.woowacourse.friendogly.member.domain;
 
-import com.woowacourse.friendogly.exception.FriendoglyException;
-import io.micrometer.common.util.StringUtils;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Member {
-
-    private static final int MAX_NAME_LENGTH = 15;
-    private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
-            Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Embedded
+    private Name name;
 
-    private String email;
+    @Embedded
+    private Email email;
 
     @Builder
     public Member(String name, String email) {
-        validateName(name);
-        validateEmail(email);
-
-        this.name = name;
-        this.email = email;
-    }
-
-    private void validateName(String value) {
-        if (StringUtils.isBlank(value) || value.length() > MAX_NAME_LENGTH) {
-            throw new FriendoglyException("올바른 이름 형식이 아닙니다.");
-        }
-    }
-
-    private void validateEmail(String value) {
-        if (StringUtils.isBlank(value) || !VALID_EMAIL_ADDRESS_REGEX.matcher(value).matches()) {
-            throw new FriendoglyException("올바른 이메일 형식이 아닙니다.");
-        }
+        this.name = new Name(name);
+        this.email = new Email(email);
     }
 }
--- backend/src/main/java/com/woowacourse/friendogly/member/dto/response/SaveMemberResponse.java
+++ backend/src/main/java/com/woowacourse/friendogly/member/dto/response/SaveMemberResponse.java
@@ -5,6 +5,6 @@ import com.woowacourse.friendogly.member.domain.Member;
 public record SaveMemberResponse(Long id, String name, String email) {
 
     public SaveMemberResponse(Member member) {
-        this(member.getId(), member.getName(), member.getEmail());
+        this(member.getId(), member.getName().getValue(), member.getEmail().getValue());
     }
 }
