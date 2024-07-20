22a67286e4838d6b08213d4a96b110b33edf051a
김진우 <jinwoo3661@naver.com>
2024-07-20 23:34:51 +0900

    feat: fragment viewPager 구현

---
+++ android/app/src/main/java/com/woowacourse/friendogly/presentation/ui/group/add/GroupAddNavigationAction.kt
@@ -0,0 +1,6 @@
+package com.woowacourse.friendogly.presentation.ui.group.add
+
+sealed interface GroupAddNavigationAction {
+    data object NavigateToHome: GroupAddNavigationAction
+    data object NavigateToGroupPoster: GroupAddNavigationAction
+}
