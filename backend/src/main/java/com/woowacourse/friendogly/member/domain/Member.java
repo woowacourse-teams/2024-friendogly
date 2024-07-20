e09adcb9ea38efa2c249e93f8be50430b2d9c58f
J-I-H-O <jeongjiho0731@gmail.com>
2024-07-20 12:27:00 +0900

    refactor: 회원 및 반려견 양방향 연관관계 설정

--- backend/src/main/java/com/woowacourse/friendogly/member/domain/Member.java
+++ backend/src/main/java/com/woowacourse/friendogly/member/domain/Member.java
@@ -1,14 +1,19 @@
 package com.woowacourse.friendogly.member.domain;
 
+import com.woowacourse.friendogly.pet.domain.Pet;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,6 +24,9 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(mappedBy = "member")
+    private List<Pet> pets;
+
     @Embedded
     private Name name;
 
@@ -27,6 +35,7 @@ public class Member {
 
     @Builder
     public Member(String name, String email) {
+        this.pets = new ArrayList<>();
         this.name = new Name(name);
         this.email = new Email(email);
     }
