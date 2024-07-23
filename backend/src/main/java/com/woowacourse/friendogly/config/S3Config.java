fc0c75ace1d62a38b5bff3446df90c3fdea5ea8a
ehtjsv2 <ehtjsv2@naver.com>
2024-07-24 08:59:04 +0900

    feat: s3 인증 정보 매개변수 변경

--- backend/src/main/java/com/woowacourse/friendogly/config/S3Config.java
+++ backend/src/main/java/com/woowacourse/friendogly/config/S3Config.java
@@ -1,5 +1,6 @@
 package com.woowacourse.friendogly.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -13,9 +14,10 @@ import org.springframework.context.annotation.Profile;
 public class S3Config {
 
     @Bean
-    public AmazonS3 S3Client(){
+    public AmazonS3 S3Client() {
         return AmazonS3ClientBuilder.standard()
-                .withCredentials(InstanceProfileCredentialsProvider.getInstance())
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        InstanceProfileCredentialsProvider.getInstance().getCredentials()))
                 .withRegion(Regions.AP_NORTHEAST_2)
                 .build();
     }
