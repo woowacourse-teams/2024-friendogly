5635602617f4af332d304588c3fdb1b8a575f7da
JunJangE <wnswkd486@gmail.com>
2024-07-23 14:47:58 +0900

    chore: 필요없는 코드 제거

--- android/app/src/main/java/com/woowacourse/friendogly/remote/mapper/MemberDtoMapper.kt
+++ android/app/src/main/java/com/woowacourse/friendogly/remote/mapper/MemberDtoMapper.kt
@@ -1,11 +1,9 @@
 package com.woowacourse.friendogly.remote.mapper
 
-import android.util.Log
 import com.woowacourse.friendogly.data.model.MemberDto
 import com.woowacourse.friendogly.remote.model.response.MemberResponse
 
 fun MemberResponse.toData(): MemberDto {
-    Log.d("ttt this", this.toString())
     return MemberDto(
         id = id,
     )
