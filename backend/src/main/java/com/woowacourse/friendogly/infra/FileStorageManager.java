417be4aa0437e4b048bf0194c76c70ed03410d08
ehtjsv2 <ehtjsv2@naver.com>
2024-07-24 11:54:02 +0900

    refactor: interface의 메소드에 public 제거

--- backend/src/main/java/com/woowacourse/friendogly/infra/FileStorageManager.java
+++ backend/src/main/java/com/woowacourse/friendogly/infra/FileStorageManager.java
@@ -4,5 +4,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FileStorageManager {
 
-    public String uploadFile(MultipartFile file);
+    String uploadFile(MultipartFile file);
 }
