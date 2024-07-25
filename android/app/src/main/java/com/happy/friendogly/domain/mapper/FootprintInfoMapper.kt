51e9f7d3981061b5be036ad5bfaaaab856046169
dpcks0509 <dpcks0509@gmail.com>
2024-07-25 15:02:01 +0900

    fix: 발자국 찍은 시간 버그 수정

--- android/app/src/main/java/com/happy/friendogly/domain/mapper/FootprintInfoMapper.kt
+++ android/app/src/main/java/com/happy/friendogly/domain/mapper/FootprintInfoMapper.kt
@@ -54,8 +54,8 @@ fun LocalDateTime.toDateOfVisit(
 
     return when {
         days > 0 -> "${days}일 전"
-        hours > 0 -> "${hours}일 전"
-        minutes > 0 -> "${minutes}일 전"
+        hours > 0 -> "${hours}시간 전"
+        minutes > 0 -> "${minutes}분 전"
         else -> "방금 전"
     }
 }
