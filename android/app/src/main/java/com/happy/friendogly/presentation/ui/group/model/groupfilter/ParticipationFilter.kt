8ea6c55aa19a2e94046e96e1abbe3eec2b9441bb
김진우 <jinwoo3661@naver.com>
2024-07-27 20:23:41 +0900

    feat: find participationfilter 기능 구현

--- android/app/src/main/java/com/happy/friendogly/presentation/ui/group/model/groupfilter/ParticipationFilter.kt
+++ android/app/src/main/java/com/happy/friendogly/presentation/ui/group/model/groupfilter/ParticipationFilter.kt
@@ -3,5 +3,13 @@ package com.happy.friendogly.presentation.ui.group.model.groupfilter
 enum class ParticipationFilter(val filterName: String) {
     POSSIBLE("참여 가능"),
     RECRUITMENT("모집중"),
-    ENTIRE("전체"),
+    ENTIRE("전체");
+
+    companion object {
+        fun findParticipationFilter(filterName:String): ParticipationFilter?{
+            return ParticipationFilter.entries.find {
+                it.filterName == filterName
+            }
+        }
+    }
 }
