95bebd95b9e1bc545bb1276b72fdbd9cd891802f
JunJangE <wnswkd486@gmail.com>
2024-07-27 19:36:50 +0900

    feat: 상태 코드와 에러코드에 따른 예외 처리

--- android/app/src/main/java/com/happy/friendogly/remote/interceptor/ErrorResponseInterceptor.kt
+++ android/app/src/main/java/com/happy/friendogly/remote/interceptor/ErrorResponseInterceptor.kt
@@ -1,12 +1,7 @@
 package com.happy.friendogly.remote.interceptor
 
-import com.happy.friendogly.remote.model.error.BadRequestException
-import com.happy.friendogly.remote.model.error.ErrorResponseImpl
-import com.happy.friendogly.remote.model.error.InternalServerErrorException
-import com.happy.friendogly.remote.model.error.InvalidAccessTokenException
-import com.happy.friendogly.remote.model.error.InvalidAccessTokenExpire
-import com.happy.friendogly.remote.model.error.ServerNotFoundException
-import kotlinx.serialization.json.Json
+import com.happy.friendogly.remote.model.error.createErrorException
+import com.happy.friendogly.remote.model.error.createErrorResponse
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -39,24 +34,3 @@ class ErrorResponseInterceptor : Interceptor {
         }
     }
 }
-
-fun createErrorResponse(responseBodyString: String): ErrorResponseImpl? =
-    try {
-        Json.decodeFromString<ErrorResponseImpl>(responseBodyString)
-    } catch (e: Exception) {
-        null
-    }
-
-fun createErrorException(
-    url: String?,
-    httpCode: Int,
-    errorResponse: ErrorResponseImpl?,
-): Exception? =
-    when (httpCode) {
-        400 -> BadRequestException(Throwable(errorResponse?.reason), url)
-        401 -> InvalidAccessTokenExpire(Throwable(errorResponse?.reason), url)
-        403 -> InvalidAccessTokenException(Throwable(errorResponse?.reason), url)
-        404 -> ServerNotFoundException(Throwable(errorResponse?.reason), url)
-        500 -> InternalServerErrorException(Throwable(errorResponse?.reason), url)
-        else -> null
-    }
--- android/app/src/main/java/com/happy/friendogly/remote/model/error/ErrorResponse.kt
+++ android/app/src/main/java/com/happy/friendogly/remote/model/error/ErrorResponse.kt
@@ -1,9 +1,23 @@
 package com.happy.friendogly.remote.model.error
 
-interface ErrorResponse {
-    val success: Boolean
-    val status: Int
-    val reason: String
-    val timeStamp: String
-    val path: String
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ErrorResponse(
+    val isSuccess: Boolean,
+    val data: ErrorData,
+)
+
+@Serializable
+data class ErrorData(
+    val errorCode: String, // TODO 아직 에러 코드 문서화가 안 된 상태이기 때문에 String으로 받음
+    val errorMessage: String,
+    val detail: List<String>,
+)
+
+@Serializable
+enum class ErrorCode {
+    @SerializedName("DEFAULT_ERROR_CODE")
+    DEFAULT_ERROR_CODE,
 }
