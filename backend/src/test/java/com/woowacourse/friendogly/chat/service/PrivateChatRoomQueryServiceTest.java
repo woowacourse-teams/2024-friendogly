c406c6c30b3eecdc2cc19f6c0b18115fc3840b02
takoyakimchi <c4nyou@gmail.com>
2024-07-31 16:01:00 +0900

    fix: 1대1 채팅방 전체조회 응답 DTO에서 채팅 상대방의 이름을 응답하도록 변경

--- backend/src/test/java/com/woowacourse/friendogly/chat/service/PrivateChatRoomQueryServiceTest.java
+++ backend/src/test/java/com/woowacourse/friendogly/chat/service/PrivateChatRoomQueryServiceTest.java
@@ -1,6 +1,7 @@
 package com.woowacourse.friendogly.chat.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.woowacourse.friendogly.chat.domain.PrivateChatRoom;
 import com.woowacourse.friendogly.chat.dto.response.FindMyPrivateChatRoomResponse;
@@ -8,6 +9,7 @@ import com.woowacourse.friendogly.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 class PrivateChatRoomQueryServiceTest extends ChatRoomServiceTest {
 
@@ -15,6 +17,7 @@ class PrivateChatRoomQueryServiceTest extends ChatRoomServiceTest {
     private PrivateChatRoomQueryService privateChatRoomQueryService;
 
     @DisplayName("내가 속해 있는 1:1 채팅방을 찾을 수 있다.")
+    @Transactional
     @Test
     void findMine() {
         // given
@@ -23,10 +26,16 @@ class PrivateChatRoomQueryServiceTest extends ChatRoomServiceTest {
 
         PrivateChatRoom r1 = privateChatRoomRepository.save(new PrivateChatRoom(member1, member2));
         PrivateChatRoom r2 = privateChatRoomRepository.save(new PrivateChatRoom(member1, member3));
+        r2.leave(member3);
 
         // when - then
-        assertThat(privateChatRoomQueryService.findMine(member1.getId()))
-                .extracting(FindMyPrivateChatRoomResponse::privateChatRoomId)
-                .containsExactly(r1.getId(), r2.getId());
+        assertAll(
+                () -> assertThat(privateChatRoomQueryService.findMine(member1.getId()))
+                        .extracting(FindMyPrivateChatRoomResponse::privateChatRoomId)
+                        .containsExactly(r1.getId(), r2.getId()),
+                () -> assertThat(privateChatRoomQueryService.findMine(member1.getId()))
+                        .extracting(FindMyPrivateChatRoomResponse::oppositeMemberName)
+                        .containsExactly(member2.getName().getValue(), "")
+        );
     }
 }
