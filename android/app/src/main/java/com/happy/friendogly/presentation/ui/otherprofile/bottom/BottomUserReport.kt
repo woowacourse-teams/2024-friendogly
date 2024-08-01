c664fe12fa0bdcda001bf7bfa11dda8020785867
JunJangE <wnswkd486@gmail.com>
2024-08-01 15:16:13 +0900

    refactor: 신고하기 edit text 포커스 관리

--- android/app/src/main/java/com/happy/friendogly/presentation/ui/otherprofile/bottom/BottomUserReport.kt
+++ android/app/src/main/java/com/happy/friendogly/presentation/ui/otherprofile/bottom/BottomUserReport.kt
@@ -6,16 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.happy.friendogly.R
+import com.happy.friendogly.databinding.BottomSheetUserReportBinding
 import com.happy.friendogly.presentation.utils.hideKeyboard
 import com.happy.friendogly.presentation.utils.messageTextOnFocusChangeListener
 import com.happy.friendogly.presentation.utils.showKeyboard
@@ -25,31 +21,14 @@ class BottomUserReport(
 ) : BottomSheetDialogFragment() {
     private lateinit var dlg: BottomSheetDialog
 
+    private lateinit var binding: BottomSheetUserReportBinding
+
     private var reportType: ReportType = ReportType.Init
-
-    private val reportOne by lazy { requireView().findViewById<ConstraintLayout>(R.id.constraintLayout_report_one) }
-    private val reportOneCheckBox by lazy { requireView().findViewById<CheckBox>(R.id.checkBox_report_one) }
-
-    private val reportTwo by lazy { requireView().findViewById<ConstraintLayout>(R.id.constraintLayout_report_two) }
-    private val reportTwoCheckBox by lazy { requireView().findViewById<CheckBox>(R.id.checkBox_report_two) }
-
-    private val reportThree by lazy { requireView().findViewById<ConstraintLayout>(R.id.constraintLayout_report_three) }
-    private val reportThreeCheckBox by lazy { requireView().findViewById<CheckBox>(R.id.checkBox_report_three) }
-
-    private val reportFourth by lazy { requireView().findViewById<ConstraintLayout>(R.id.constraintLayout_report_fourth) }
-    private val reportFourthCheckBox by lazy { requireView().findViewById<CheckBox>(R.id.checkBox_report_foruth) }
-
-    private val reportFive by lazy { requireView().findViewById<ConstraintLayout>(R.id.constraintLayout_report_five) }
-    private val reportFiveCheckBox by lazy { requireView().findViewById<CheckBox>(R.id.checkBox_report_five) }
-
-    private val editTextReport by lazy { requireView().findViewById<EditText>(R.id.edit_text_report) }
-    private val linearLayoutEditTextReport by lazy { requireView().findViewById<LinearLayout>(R.id.linear_layout_edit_text_report) }
-
-    private val saveBtn by lazy { requireView().findViewById<TextView>(R.id.tv_report_save) }
-    private val closeBtn by lazy { requireView().findViewById<ImageView>(R.id.iv_close) }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         dlg = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        setStyle(STYLE_NORMAL, R.style.DialogStyle)
+
         dlg.setOnShowListener {
             val bottomSheet =
                 dlg.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
@@ -66,8 +45,9 @@ class BottomUserReport(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.bottom_sheet_user_report, container, false)
+    ): View {
+        binding = BottomSheetUserReportBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(
@@ -80,71 +60,77 @@ class BottomUserReport(
     }
 
     private fun initViewCreated() {
-        saveBtn.setBackgroundResource(R.drawable.rect_gray03_fill_16)
-        saveBtn.isEnabled = false
+        binding.tvReportSave.setBackgroundResource(R.drawable.rect_gray03_fill_16)
+        binding.tvReportSave.isEnabled = false
     }
 
     private fun initClickListener() {
-        saveBtn.setOnClickListener {
+        binding.tvReportSave.setOnClickListener {
             onSaved.invoke(reportType)
             dismiss()
         }
 
-        closeBtn.setOnClickListener {
+        binding.ivClose.setOnClickListener {
             dismiss()
         }
 
-        reportOne.setOnClickListener {
+        binding.constraintLayoutReportOne.setOnClickListener {
             updateReportType(ReportType.One)
         }
-        reportTwo.setOnClickListener {
+        binding.constraintLayoutReportTwo.setOnClickListener {
             updateReportType(ReportType.Two)
         }
-        reportThree.setOnClickListener {
+        binding.constraintLayoutReportThree.setOnClickListener {
             updateReportType(ReportType.Three)
         }
-        reportFourth.setOnClickListener {
+        binding.constraintLayoutReportFourth.setOnClickListener {
             updateReportType(ReportType.Fourth)
         }
-        reportFive.setOnClickListener {
+        binding.constraintLayoutReportFive.setOnClickListener {
             updateReportType(ReportType.Five)
+        }
+
+        binding.editTextReport.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                updateReportType(ReportType.Five)
+            }
         }
     }
 
     private fun updateReportType(type: ReportType) {
         when (type) {
             is ReportType.One -> {
-                checkReport(reportOneCheckBox)
-                requireActivity().hideKeyboard()
-                editTextReport.clearFocus()
+                checkReport(binding.checkBoxReportOne)
+                hideKeyboard()
+                binding.editTextReport.clearFocus()
             }
 
             is ReportType.Two -> {
-                checkReport(reportTwoCheckBox)
-                requireActivity().hideKeyboard()
-                editTextReport.clearFocus()
+                checkReport(binding.checkBoxReportTwo)
+                hideKeyboard()
+                binding.editTextReport.clearFocus()
             }
 
             is ReportType.Three -> {
-                checkReport(reportThreeCheckBox)
-                requireActivity().hideKeyboard()
-                editTextReport.clearFocus()
+                checkReport(binding.checkBoxReportThree)
+                hideKeyboard()
+                binding.editTextReport.clearFocus()
             }
 
             is ReportType.Fourth -> {
-                checkReport(reportFourthCheckBox)
-                requireActivity().hideKeyboard()
-                editTextReport.clearFocus()
+                checkReport(binding.checkBoxReportForuth)
+                hideKeyboard()
+                binding.editTextReport.clearFocus()
             }
 
             is ReportType.Five -> {
-                checkReport(reportFiveCheckBox)
-                editTextReport.messageTextOnFocusChangeListener(
+                checkReport(binding.checkBoxReportFive)
+                binding.editTextReport.messageTextOnFocusChangeListener(
                     requireContext(),
-                    linearLayoutEditTextReport,
+                    binding.linearLayoutEditTextReport,
                 )
-                requireActivity().showKeyboard(editTextReport)
-                editTextReport.requestFocus()
+                binding.editTextReport.requestFocus()
+                showKeyboard(binding.editTextReport)
             }
 
             is ReportType.Init -> Unit
@@ -153,14 +139,14 @@ class BottomUserReport(
     }
 
     private fun checkReport(selectedCheckBox: CheckBox) {
-        saveBtn.setBackgroundResource(R.drawable.rect_coral04_fill_16)
-        saveBtn.isEnabled = true
+        binding.tvReportSave.setBackgroundResource(R.drawable.rect_coral04_fill_16)
+        binding.tvReportSave.isEnabled = true
         listOf(
-            reportOneCheckBox,
-            reportTwoCheckBox,
-            reportThreeCheckBox,
-            reportFourthCheckBox,
-            reportFiveCheckBox,
+            binding.checkBoxReportOne,
+            binding.checkBoxReportTwo,
+            binding.checkBoxReportThree,
+            binding.checkBoxReportForuth,
+            binding.checkBoxReportFive,
         ).forEach { it.isChecked = false }
 
         selectedCheckBox.isChecked = true
