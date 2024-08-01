7b372e493689bcf7269f17e7f3790e36250c2f24
Jeong Jiho <jeongjiho0731@gmail.com>
2024-08-01 19:34:17 +0900

    [BE] feat: 모임 상세 조회 API 구현 및 검색 필터링 조건 추가 (#269)
    Co-authored-by: jimi567 <repday0609@gmail.com>

--- backend/src/main/java/com/woowacourse/friendogly/club/controller/ClubController.java
+++ backend/src/main/java/com/woowacourse/friendogly/club/controller/ClubController.java
@@ -4,6 +4,7 @@ import com.woowacourse.friendogly.auth.Auth;
 import com.woowacourse.friendogly.club.dto.request.FindSearchingClubRequest;
 import com.woowacourse.friendogly.club.dto.request.SaveClubMemberRequest;
 import com.woowacourse.friendogly.club.dto.request.SaveClubRequest;
+import com.woowacourse.friendogly.club.dto.response.FindClubResponse;
 import com.woowacourse.friendogly.club.dto.response.FindSearchingClubResponse;
 import com.woowacourse.friendogly.club.dto.response.SaveClubMemberResponse;
 import com.woowacourse.friendogly.club.dto.response.SaveClubResponse;
@@ -16,11 +17,14 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/clubs")
@@ -34,22 +38,31 @@ public class ClubController {
         this.clubQueryService = clubQueryService;
     }
 
+    @GetMapping("/{id}")
+    public ApiResponse<FindClubResponse> findById(@Auth Long memberId, @PathVariable Long id) {
+        return ApiResponse.ofSuccess(clubQueryService.findById(memberId, id));
+    }
+
     @GetMapping("/searching")
-    public ApiResponse<List<FindSearchingClubResponse>> findSearching(@Valid FindSearchingClubRequest request) {
-        return ApiResponse.ofSuccess(clubQueryService.findSearching(request));
+    public ApiResponse<List<FindSearchingClubResponse>> findByFilter(
+            @Auth Long memberId,
+            @Valid @ModelAttribute FindSearchingClubRequest request
+    ) {
+        return ApiResponse.ofSuccess(clubQueryService.findFindByFilter(memberId, request));
     }
 
     @PostMapping
     public ResponseEntity<ApiResponse<SaveClubResponse>> save(
             @Auth Long memberId,
-            @Valid @RequestBody SaveClubRequest request
+            @Valid @RequestPart SaveClubRequest request,
+            @RequestPart(required = false) MultipartFile image
     ) {
-        SaveClubResponse response = clubCommandService.save(memberId, request);
+        SaveClubResponse response = clubCommandService.save(memberId, image, request);
         return ResponseEntity.created(URI.create("/clubs" + response.id())).body(ApiResponse.ofSuccess(response));
     }
 
     @PostMapping("/{clubId}/members")
-    public ResponseEntity<ApiResponse<SaveClubMemberResponse>> saveClubMember(
+    public ResponseEntity<ApiResponse<SaveClubMemberResponse>> joinClub(
             @PathVariable Long clubId,
             @Auth Long memberId,
             @Valid @RequestBody SaveClubMemberRequest request
@@ -62,7 +75,7 @@ public class ClubController {
     @DeleteMapping("/{clubId}/members")
     public ResponseEntity<Void> deleteClubMember(
             @Auth Long memberId,
-            @PathVariable("clubId") Long clubId
+            @PathVariable Long clubId
     ) {
         clubCommandService.deleteClubMember(clubId, memberId);
         return ResponseEntity.noContent().build();
