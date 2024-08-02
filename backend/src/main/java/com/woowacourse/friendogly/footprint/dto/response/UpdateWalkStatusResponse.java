76b2ef754a6c2023fafe4e69d391c05ae582b2d3
ehtjsv2 <ehtjsv2@naver.com>
2024-08-02 16:43:57 +0900

    feat: 발자국 산책 상태변경 controller 메서드 및 api docs생성

---
+++ backend/src/main/java/com/woowacourse/friendogly/footprint/dto/response/UpdateWalkStatusResponse.java
@@ -0,0 +1,7 @@
+package com.woowacourse.friendogly.footprint.dto.response;
+
+import com.woowacourse.friendogly.footprint.domain.WalkStatus;
+
+public record UpdateWalkStatusResponse(WalkStatus walkStatus) {
+
+}
