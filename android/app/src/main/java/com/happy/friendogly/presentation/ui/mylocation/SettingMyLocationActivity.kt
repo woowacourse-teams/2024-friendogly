580584d6d7d5ec9da9a235111525f0b0c49cba69
김진우 <jinwoo3661@naver.com>
2024-08-02 10:38:43 +0900

    feat: SettingMyLocationActivity 구체화

--- android/app/src/main/java/com/happy/friendogly/presentation/ui/mylocation/SettingMyLocationActivity.kt
+++ android/app/src/main/java/com/happy/friendogly/presentation/ui/mylocation/SettingMyLocationActivity.kt
@@ -1,25 +1,174 @@
 package com.happy.friendogly.presentation.ui.mylocation
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import com.airbnb.lottie.LottieAnimationView
 import com.happy.friendogly.R
 import com.happy.friendogly.application.di.AppModule
 import com.happy.friendogly.databinding.ActivitySettingMyLocationBinding
 import com.happy.friendogly.presentation.base.BaseActivity
+import com.happy.friendogly.presentation.ui.MainActivity
+import com.happy.friendogly.presentation.ui.permission.LocationPermission
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
 
 class SettingMyLocationActivity :
-    BaseActivity<ActivitySettingMyLocationBinding>(R.layout.activity_setting_my_location) {
+    BaseActivity<ActivitySettingMyLocationBinding>(R.layout.activity_setting_my_location),
+    OnMapReadyCallback {
+    private lateinit var map: NaverMap
+    private lateinit var latLng: LatLng
+    private val mapView: MapView by lazy { binding.mapViewMyLocation }
+    private val loadingView: LottieAnimationView by lazy { binding.lottieMyLocationLoading }
+    private val locationPermission: LocationPermission = initLocationPermission()
 
-    private val viewModel: SettingMyLocationViewModel by viewModels()
+    private val viewModel: SettingMyLocationViewModel by viewModels<SettingMyLocationViewModel> {
+        SettingMyLocationViewModel.factory(
+            getAddressUseCase = AppModule.getInstance().getAddressUseCase
+        )
+    }
+
+    private val locationSource: FusedLocationSource by lazy {
+        FusedLocationSource(
+            this,
+            MainActivity.LOCATION_PERMISSION_REQUEST_CODE,
+        )
+    }
 
     override fun initCreateView() {
+        requestUserPermission()
+        startAnimation()
         initDataBinding()
         initObserver()
     }
 
-    private fun initDataBinding() {
+    private fun requestUserPermission(){
+        if (!locationPermission.hasPermissions()){
+            locationPermission.createAlarmDialog().show(supportFragmentManager,"TAG")
+        }
     }
 
-    private fun initObserver(){
+    private fun startAnimation() {
+        loadingView.playAnimation()
+    }
 
+    private fun cancelAnimation() {
+        binding.lottieMyLocationLoading.isVisible = false
+        binding.lottieMyLocationLoading.cancelAnimation()
+    }
+
+    private fun initDataBinding() {
+        binding.vm = viewModel
+    }
+
+    private fun initObserver() {
+
+    }
+
+    private fun initLocationPermission() =
+        LocationPermission.from(this) { isPermitted ->
+            if (isPermitted) {
+                activateMap()
+            } else {
+                showSnackbar(getString(R.string.permission_denied_message))
+                map.locationTrackingMode =
+                    LocationTrackingMode.NoFollow
+            }
+        }
+
+    private fun activateMap() {
+        locationSource.activate { location ->
+            val lastLocation = location ?: return@activate
+            latLng = LatLng(lastLocation.latitude, lastLocation.longitude)
+            moveCameraCenterPosition()
+            Handler(Looper.getMainLooper()).postDelayed(
+                {
+                    cancelAnimation()
+                },
+                1000,
+            )
+            initMarker()
+        }
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        initMap(naverMap)
+        activateMap()
+    }
+
+    private fun moveCameraCenterPosition() {
+        val cameraUpdate = CameraUpdate.scrollAndZoomTo(latLng, DEFAULT_ZOOM)
+        map.moveCamera(cameraUpdate)
+        map.locationTrackingMode = LocationTrackingMode.Follow
+    }
+
+    private fun initMarker(){
+        val marker = Marker()
+        marker.position = latLng
+        marker.icon = OverlayImage.fromResource(R.drawable.ic_marker_mine_clicked)
+        marker.width = MARKER_WIDTH
+        marker.height = MARKER_HEIGHT
+        marker.map = map
+    }
+
+    private fun initMap(map: NaverMap) {
+        this.map = map
+        map.locationSource = locationSource
+        map.uiSettings.apply {
+            isScrollGesturesEnabled = false
+            isZoomGesturesEnabled = false
+            isTiltGesturesEnabled = false
+            isRotateGesturesEnabled = false
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    companion object {
+        private const val DEFAULT_ZOOM = 13.5
+        private const val MARKER_WIDTH = 140
+        private const val MARKER_HEIGHT = 180
     }
 }
--- android/app/src/main/java/com/happy/friendogly/presentation/ui/mylocation/SettingMyLocationViewModel.kt
+++ android/app/src/main/java/com/happy/friendogly/presentation/ui/mylocation/SettingMyLocationViewModel.kt
@@ -2,15 +2,32 @@ package com.happy.friendogly.presentation.ui.mylocation
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
 import com.happy.friendogly.domain.model.Address
+import com.happy.friendogly.domain.usecase.GetAddressUseCase
 import com.happy.friendogly.presentation.base.BaseViewModel
+import com.happy.friendogly.presentation.base.BaseViewModelFactory
 
-class SettingMyLocationViewModel : BaseViewModel(), SettingMyLocationActionHandler {
+class SettingMyLocationViewModel(
+    private val getAddressUseCase: GetAddressUseCase,
+) : BaseViewModel(), SettingMyLocationActionHandler {
 
     private val _address: MutableLiveData<Address> = MutableLiveData()
     val address: LiveData<Address> get() = _address
 
-    fun updateAddress(address: Address){
+    fun updateAddress(address: Address) {
         _address.value = address
+    }
+
+    companion object {
+        fun factory(
+            getAddressUseCase: GetAddressUseCase,
+        ): ViewModelProvider.Factory {
+            return BaseViewModelFactory {
+                SettingMyLocationViewModel(
+                    getAddressUseCase = getAddressUseCase
+                )
+            }
+        }
     }
 }
