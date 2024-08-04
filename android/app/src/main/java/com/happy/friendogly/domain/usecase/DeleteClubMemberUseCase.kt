d0932324eb3fd10d654c821ae45e98aae4ae0e16
김진우 <jinwoo3661@naver.com>
2024-08-05 00:53:22 +0900

    feat: AppModule 적용

--- android/app/src/main/java/com/happy/friendogly/domain/usecase/DeleteClubMemberUseCase.kt
+++ android/app/src/main/java/com/happy/friendogly/domain/usecase/DeleteClubMemberUseCase.kt
@@ -2,7 +2,7 @@ package com.happy.friendogly.domain.usecase
 
 import com.happy.friendogly.domain.repository.ClubRepository
 
-class DeleteClubUseCase(
+class DeleteClubMemberUseCase(
     private val repository: ClubRepository,
 ) {
     suspend operator fun invoke(id: Long): Result<Unit> =
