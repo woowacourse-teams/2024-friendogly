d91e8ce61955a970e84147936b0d94074bb22c51
ehtjsv2 <ehtjsv2@naver.com>
2024-08-08 16:07:01 +0900

    refactor: firebase secret json위치 ClassPath기준으로 변경

--- backend/src/main/java/com/woowacourse/friendogly/config/FirebaseAppConfig.java
+++ backend/src/main/java/com/woowacourse/friendogly/config/FirebaseAppConfig.java
@@ -3,18 +3,24 @@ package com.woowacourse.friendogly.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class FirebaseAppConfig {
 
+    @Value("${file.firebase.path}")
+    private String FIREBASE_FILE_PATH;
+
     @Bean
     FirebaseApp firebaseApp() throws IOException {
-        FileInputStream credentialsStream = new FileInputStream(
-                "src/main/resources/firebase-friendogly-private-key.json");
+        Resource resource = new ClassPathResource(FIREBASE_FILE_PATH);
+        InputStream credentialsStream = resource.getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(credentialsStream))
