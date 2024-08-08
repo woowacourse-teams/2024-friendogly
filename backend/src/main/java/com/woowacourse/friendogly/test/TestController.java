0a7c180efe64ce3c4e815eea74edd82d00d794e3
takoyakimchi <c4nyou@gmail.com>
2024-08-08 13:41:56 +0900

    feat: 로깅 테스트용 컨트롤러 추가

--- backend/src/main/java/com/woowacourse/friendogly/test/TestController.java
+++ backend/src/main/java/com/woowacourse/friendogly/test/TestController.java
@@ -1,14 +1,34 @@
 package com.woowacourse.friendogly.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @GetMapping("/hello")
     public String hello() {
         // TODO: 추후 삭제하기!
         return "Hello World!";
+    }
+
+    @GetMapping("/log/info")
+    public String logInfo() {
+        log.info("--- INFO 로그 남기기 테스트 ---");
+        return "info";
+    }
+
+    @GetMapping("/log/error")
+    public String logError() {
+        log.error("--- ERROR 로그 남기기 테스트 ---");
+        return "error";
+    }
+
+    @GetMapping("/log/warn")
+    public String logWarn() {
+        log.info("--- WARN 로그 남기기 테스트 ---");
+        return "warn";
     }
 }
