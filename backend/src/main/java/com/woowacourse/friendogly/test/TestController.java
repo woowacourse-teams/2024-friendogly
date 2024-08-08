8d6234f5ec7666081d6cd8c789c74148e59fb0fa
Jeong Jiho <jeongjiho0731@gmail.com>
2024-08-09 08:54:10 +0900

    [BE] chore: spring monitoring (#337)
    Co-authored-by: jimi567 <repday0609@gmail.com>

--- backend/src/main/java/com/woowacourse/friendogly/test/TestController.java
+++ backend/src/main/java/com/woowacourse/friendogly/test/TestController.java
@@ -1,14 +1,25 @@
 package com.woowacourse.friendogly.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        // TODO: 추후 삭제하기!
-        return "Hello World!";
+    @GetMapping("/log/info")
+    public void info() {
+        log.info("INFO LOG!!!");
+    }
+
+    @GetMapping("/log/warn")
+    public void warn() {
+        log.warn("WARN LOG!!!");
+    }
+
+    @GetMapping("/log/error")
+    public void error() {
+        log.error("ERROR LOG!!!");
     }
 }
