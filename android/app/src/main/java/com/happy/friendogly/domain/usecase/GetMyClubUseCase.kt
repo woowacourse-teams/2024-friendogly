3e0039130d845286bfd49c6a7713cf840d9ea252
김진우 <jinwoo3661@naver.com>
2024-08-11 01:13:08 +0900

    feat: my club usecase 구현

---
+++ android/app/src/main/java/com/happy/friendogly/domain/usecase/GetMyClubUseCase.kt
@@ -0,0 +1,11 @@
+package com.happy.friendogly.domain.usecase
+
+import com.happy.friendogly.domain.model.Club
+import com.happy.friendogly.domain.repository.MyClubRepository
+
+class GetMyClubUseCase(
+    private val repository: MyClubRepository,
+) {
+    suspend operator fun invoke(): Result<List<Club>> =
+        repository.getMyClubs()
+}
---
+++ android/app/src/main/java/com/happy/friendogly/domain/usecase/GetMyHeadClubUseCase.kt
@@ -0,0 +1,11 @@
+package com.happy.friendogly.domain.usecase
+
+import com.happy.friendogly.domain.model.Club
+import com.happy.friendogly.domain.repository.MyClubRepository
+
+class GetMyHeadClubUseCase(
+    private val repository: MyClubRepository,
+) {
+    suspend operator fun invoke(): Result<List<Club>> =
+        repository.getMyHeadClubs()
+}
