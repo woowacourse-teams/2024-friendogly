2a998044e7fb77f00157ca01722b60f6b3f97c41
JunJangE <wnswkd486@gmail.com>
2024-08-10 12:15:05 +0900

    chore: v0.2.1 업데이트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.happy.friendogly"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
