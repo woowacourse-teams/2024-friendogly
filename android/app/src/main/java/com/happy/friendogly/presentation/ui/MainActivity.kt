ee4122ae4c892fa1a3f8dee4dcf956ae4404e587
gaeun <geun5744@gmail.com>
2024-08-11 18:18:41 +0900

    fix: container가 null인 경우에만 뷰 초기화 되도록 구현

--- android/app/src/main/java/com/happy/friendogly/presentation/ui/MainActivity.kt
+++ android/app/src/main/java/com/happy/friendogly/presentation/ui/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity :
     }
 
     private fun initNavController() {
-        switchFragment(ClubListFragment::class.java)
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) ?: switchFragment(ClubListFragment::class.java)
         binding.bottomNavi.setOnItemReselectedListener {}
         binding.bottomNavi.setOnItemSelectedListener { item ->
             when (item.itemId) {
