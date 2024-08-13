95a86c60cc977dbd70020d41c6228d80a82c6919
J-I-H-O <jeongjiho0731@gmail.com>
2024-08-13 19:12:57 +0900

    refactor: 불필요한 의존성 주입 제거

--- backend/src/test/java/com/happy/friendogly/member/service/MemberCommandServiceTest.java
+++ backend/src/test/java/com/happy/friendogly/member/service/MemberCommandServiceTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.happy.friendogly.member.domain.Member;
 import com.happy.friendogly.member.dto.request.UpdateMemberRequest;
-import com.happy.friendogly.member.repository.MemberRepository;
 import com.happy.friendogly.support.ServiceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,9 +14,6 @@ public class MemberCommandServiceTest extends ServiceTest {
 
     @Autowired
     private MemberCommandService memberCommandService;
-
-    @Autowired
-    private MemberRepository memberRepository;
 
     @DisplayName("회원 이름을 변경하는 경우, 변경된 회원 이름이 조회된다.")
     @Test
