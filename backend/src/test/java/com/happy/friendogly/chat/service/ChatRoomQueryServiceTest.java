8d2c3481ddbd0ff00c57e31accc1947dc615f01b
takoyakimchi <c4nyou@gmail.com>
2024-08-13 19:35:59 +0900

    test: 불필요한 어노테이션 삭제

--- backend/src/test/java/com/happy/friendogly/chat/service/ChatRoomQueryServiceTest.java
+++ backend/src/test/java/com/happy/friendogly/chat/service/ChatRoomQueryServiceTest.java
@@ -28,10 +28,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
 class ChatRoomQueryServiceTest extends ServiceTest {
 
     @Autowired
