f4ae58576af137dfbc870f91ae8cfc1d5c604258
ehtjsv2 <ehtjsv2@naver.com>
2024-08-14 12:29:00 +0900

    feat: FirebaseAppConfig 예외 추가

--- backend/src/main/java/com/happy/friendogly/common/ErrorCode.java
+++ backend/src/main/java/com/happy/friendogly/common/ErrorCode.java
@@ -3,6 +3,8 @@ package com.happy.friendogly.common;
 public enum ErrorCode {
 
     // TODO: 추가 필요
-    DEFAULT_ERROR_CODE
+    DEFAULT_ERROR_CODE,
+    FAILED_LOAD_SERVER_FIREBASE_CREDENTIALS,
+    INVALID_FIREBASE_CREDENTIALS
     ;
 }
--- backend/src/main/java/com/happy/friendogly/config/FirebaseAppConfig.java
+++ backend/src/main/java/com/happy/friendogly/config/FirebaseAppConfig.java
@@ -1,8 +1,12 @@
 package com.happy.friendogly.config;
 
+import static com.happy.friendogly.common.ErrorCode.FAILED_LOAD_SERVER_FIREBASE_CREDENTIALS;
+import static com.happy.friendogly.common.ErrorCode.INVALID_FIREBASE_CREDENTIALS;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.happy.friendogly.exception.FriendoglyException;
 import java.io.IOException;
 import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 
 @Configuration
 public class FirebaseAppConfig {
@@ -18,18 +23,40 @@ public class FirebaseAppConfig {
     private String FIREBASE_FILE_PATH;
 
     @Bean
-    FirebaseApp firebaseApp() throws IOException {
-        Resource resource = new ClassPathResource(FIREBASE_FILE_PATH);
-        InputStream credentialsStream = resource.getInputStream();
+    FirebaseApp firebaseApp() {
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(credentialsStream))
-                .build();
+        InputStream firebaseCredentialsInputStream = getFirebaseCredentialsInputStream();
 
-        if(FirebaseApp.getApps().isEmpty()){
+        FirebaseOptions options = null;
+        try {
+            options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(firebaseCredentialsInputStream))
+                    .build();
+        } catch (IOException e) {
+            throw new FriendoglyException(
+                    "서버에서 Firebase인증을 실패했습니다.",
+                    INVALID_FIREBASE_CREDENTIALS,
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        if (FirebaseApp.getApps().isEmpty()) {
             return FirebaseApp.initializeApp(options);
         }
         return FirebaseApp.getInstance();
+    }
+
+    private InputStream getFirebaseCredentialsInputStream() {
+        Resource resource = new ClassPathResource(FIREBASE_FILE_PATH);
+        try {
+            return resource.getInputStream();
+        } catch (IOException e) {
+            throw new FriendoglyException(
+                    "서버에서 파이어베이스 인증정보 가져오기를 실패했습니다.",
+                    FAILED_LOAD_SERVER_FIREBASE_CREDENTIALS,
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
 }
