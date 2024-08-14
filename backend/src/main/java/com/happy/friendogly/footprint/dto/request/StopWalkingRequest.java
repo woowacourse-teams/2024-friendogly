3065aa3e1f4ee5eef9e20c12f96db516415d1cd6
ehtjsv2 <ehtjsv2@naver.com>
2024-08-14 19:03:22 +0900

    style: 개행추가

--- backend/src/main/java/com/happy/friendogly/footprint/dto/request/StopWalkingRequest.java
+++ backend/src/main/java/com/happy/friendogly/footprint/dto/request/StopWalkingRequest.java
@@ -3,6 +3,7 @@ package com.happy.friendogly.footprint.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record StopWalkingRequest(
+
         @NotNull
         Long footprintId
 ) {
