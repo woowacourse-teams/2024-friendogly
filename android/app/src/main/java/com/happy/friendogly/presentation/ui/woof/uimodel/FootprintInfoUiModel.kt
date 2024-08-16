aa015f6e8902d4ce08f11dc896badc9392ac3956
dpcks0509 <dpcks0509@gmail.com>
2024-08-16 17:40:30 +0900

    refactor: viewModel 리팩토링

---
+++ android/app/src/main/java/com/happy/friendogly/presentation/ui/woof/uimodel/FootprintInfoUiModel.kt
@@ -0,0 +1,6 @@
+package com.happy.friendogly.presentation.ui.woof.uimodel
+
+data class FootprintInfoUiModel(
+    val walkStatusInfo: WalkStatusInfoUiModel,
+    val petsDetailInfo: List<PetDetailInfoUiModel>
+)
