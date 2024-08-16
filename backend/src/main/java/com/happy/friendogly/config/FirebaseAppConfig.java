140b000b4eff6b3565c9620f3bc90b357213ef94
ehtjsv2 <ehtjsv2@naver.com>
2024-08-16 13:45:00 +0900

    refactor: local에서 secret json없이도 test통과되도록 변경

--- backend/src/main/java/com/happy/friendogly/config/FirebaseAppConfig.java
+++ backend/src/main/java/com/happy/friendogly/config/FirebaseAppConfig.java
@@ -12,11 +12,13 @@ import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 
 @Configuration
+@Profile("!local")
 public class FirebaseAppConfig {
 
     @Value("${file.firebase.path}")
