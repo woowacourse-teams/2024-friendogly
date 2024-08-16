da20dd4f6aee03d84d933644da73c8795d42d50d
ehtjsv2 <ehtjsv2@naver.com>
2024-08-16 14:48:57 +0900

    refactor: UpdateDeviceTokenRequest 메세지 명확하게 변경

--- backend/src/main/java/com/happy/friendogly/notification/dto/request/UpdateDeviceTokenRequest.java
+++ backend/src/main/java/com/happy/friendogly/notification/dto/request/UpdateDeviceTokenRequest.java
@@ -3,7 +3,7 @@ package com.happy.friendogly.notification.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdateDeviceTokenRequest(
-        @NotBlank(message = "빈 문자열이나 null을 입력할 수 없습니다.")
+        @NotBlank(message = "디바이스 토큰은 빈 문자열이나 null을 입력할 수 없습니다.")
         String deviceToken
 ) {
 
