1b39a28f9e874ce3cd46d846b00a50be1dec3b32
J-I-H-O <jeongjiho0731@gmail.com>
2024-08-16 17:15:17 +0900

    chore: 불필요한 클래스 제거

--- backend/src/main/java/com/happy/friendogly/auth/dto/KakaoLogoutRequest.java
+++
@@ -1,8 +0,0 @@
-package com.happy.friendogly.auth.dto;
-
-public record KakaoLogoutRequest(String target_id_type, Long target_id) {
-
-    public KakaoLogoutRequest(Long target_id) {
-        this("user_id", target_id);
-    }
-}
