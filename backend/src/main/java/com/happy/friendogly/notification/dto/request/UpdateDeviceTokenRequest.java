197e8e89174c6896457a5d7ac583fea1917317de
ehtjsv2 <ehtjsv2@naver.com>
2024-08-16 12:56:46 +0900

    feat: UpdateDeviceTokenRequest NotBlank추가

--- backend/src/main/java/com/happy/friendogly/notification/dto/request/UpdateDeviceTokenRequest.java
+++ backend/src/main/java/com/happy/friendogly/notification/dto/request/UpdateDeviceTokenRequest.java
@@ -1,5 +1,10 @@
 package com.happy.friendogly.notification.dto.request;
 
-public record UpdateDeviceTokenRequest(String deviceToken) {
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateDeviceTokenRequest(
+        @NotBlank(message = "빈 문자열이나 null을 입력할 수 없습니다.")
+        String deviceToken
+) {
 
 }
