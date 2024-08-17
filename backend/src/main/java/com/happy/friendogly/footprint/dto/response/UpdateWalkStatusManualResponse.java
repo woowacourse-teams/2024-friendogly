02ca2fdf7e0ba1b3cdd8c839fd7872cb5a2723cb
ehtjsv2 <ehtjsv2@naver.com>
2024-08-17 18:09:16 +0900

    style: 줄바꿈 추가

--- backend/src/main/java/com/happy/friendogly/footprint/dto/response/UpdateWalkStatusManualResponse.java
+++ backend/src/main/java/com/happy/friendogly/footprint/dto/response/UpdateWalkStatusManualResponse.java
@@ -3,5 +3,8 @@ package com.happy.friendogly.footprint.dto.response;
 import com.happy.friendogly.footprint.domain.WalkStatus;
 import java.time.LocalDateTime;
 
-public record UpdateWalkStatusManualResponse(WalkStatus walkStatus, LocalDateTime changedWalkStatusTime) {
+public record UpdateWalkStatusManualResponse(
+        WalkStatus walkStatus,
+        LocalDateTime changedWalkStatusTime
+) {
 }
