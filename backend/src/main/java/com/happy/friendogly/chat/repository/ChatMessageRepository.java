65dfc14525fdc46c5df21e76ca5d903f37aed207
takoyakimchi <c4nyou@gmail.com>
2024-09-10 16:49:03 +0900

    feat: N+1 문제 해결

--- backend/src/main/java/com/happy/friendogly/chat/repository/ChatMessageRepository.java
+++ backend/src/main/java/com/happy/friendogly/chat/repository/ChatMessageRepository.java
@@ -3,14 +3,16 @@ package com.happy.friendogly.chat.repository;
 import com.happy.friendogly.chat.domain.ChatMessage;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
-    // TODO: N+1 문제 해결
+    @EntityGraph(attributePaths = {"senderMember"}, type = EntityGraphType.FETCH)
     List<ChatMessage> findAllByChatRoomIdOrderByCreatedAtAsc(Long chatRoomId);
 
-    // TODO: N+1 문제 해결
+    @EntityGraph(attributePaths = {"senderMember"}, type = EntityGraphType.FETCH)
     List<ChatMessage> findAllByChatRoomIdAndCreatedAtAfterOrderByCreatedAtAsc(Long chatRoomId, LocalDateTime since);
 
     default List<ChatMessage> findRecent(Long chatRoomId, LocalDateTime since) {
