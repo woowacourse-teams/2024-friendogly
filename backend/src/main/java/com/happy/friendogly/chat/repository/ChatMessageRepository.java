7c35b8e7949e4daacd80072dc63252675e4b409d
takoyakimchi <c4nyou@gmail.com>
2024-09-11 10:46:22 +0900

    refactor: EntityGraphType을 명시하지 않도록 수정
    EntityGraph를 사용하는 경우 EntityGraphType의 default 값이 FETCH로 설정되어 있다.

--- backend/src/main/java/com/happy/friendogly/chat/repository/ChatMessageRepository.java
+++ backend/src/main/java/com/happy/friendogly/chat/repository/ChatMessageRepository.java
@@ -4,15 +4,14 @@ import com.happy.friendogly.chat.domain.ChatMessage;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
-    @EntityGraph(attributePaths = {"senderMember"}, type = EntityGraphType.FETCH)
+    @EntityGraph(attributePaths = {"senderMember"})
     List<ChatMessage> findAllByChatRoomIdOrderByCreatedAtAsc(Long chatRoomId);
 
-    @EntityGraph(attributePaths = {"senderMember"}, type = EntityGraphType.FETCH)
+    @EntityGraph(attributePaths = {"senderMember"})
     List<ChatMessage> findAllByChatRoomIdAndCreatedAtAfterOrderByCreatedAtAsc(Long chatRoomId, LocalDateTime since);
 
     default List<ChatMessage> findRecent(Long chatRoomId, LocalDateTime since) {
