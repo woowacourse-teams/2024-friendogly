98a4201fdd4f055a144749e18e2fcb36a3883291
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-09-16 17:51:31 +0900

    feat: prod Profile에만 해당 설정이 적용되도록 변경

--- backend/src/main/java/com/happy/friendogly/config/datasource/ReplicationRoutingDataSource.java
+++ backend/src/main/java/com/happy/friendogly/config/datasource/ReplicationRoutingDataSource.java
@@ -1,8 +1,10 @@
 package com.happy.friendogly.config.datasource;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Profile("prod")
 public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
 
     @Override
