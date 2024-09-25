bb4718cf0640ac1778b11739b4a200231aa4edc8
gaeun5744 <geun5744@gmail.com>
2024-09-25 17:12:18 +0900

    feat: NetworkModule 구현

--- android/app/src/main/java/com/happy/friendogly/remote/api/Authenticator.kt
+++ android/app/src/main/java/com/happy/friendogly/remote/api/Authenticator.kt
@@ -10,43 +10,46 @@ import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
+import javax.inject.Inject
 
-class Authenticator(
-    private val authService: AuthService,
-    private val tokenManager: TokenManager,
-    private val authenticationListener: AuthenticationListener,
-) : Authenticator {
-    override fun authenticate(
-        route: Route?,
-        response: Response,
-    ): Request? {
-        val refreshToken =
-            runBlocking {
-                tokenManager.refreshToken.first()
+class Authenticator
+    @Inject
+    constructor(
+        private val authService: AuthService,
+        private val tokenManager: TokenManager,
+        private val authenticationListener: AuthenticationListener,
+    ) : Authenticator {
+        override fun authenticate(
+            route: Route?,
+            response: Response,
+        ): Request? {
+            val refreshToken =
+                runBlocking {
+                    tokenManager.refreshToken.first()
+                }
+
+            if (refreshToken.isBlank()) {
+                authenticationListener.onSessionExpired()
+                return null
             }
+            return runBlocking {
+                refresh(refreshToken).fold(
+                    onSuccess = {
+                        tokenManager.saveAccessToken(it.accessToken)
+                        tokenManager.saveRefreshToken(it.refreshToken)
+                        AuthorizationInterceptor.from(response.request, it.accessToken)
+                    },
+                    onFailure = {
+                        authenticationListener.onSessionExpired()
+                        null
+                    },
+                )
+            }
+        }
 
-        if (refreshToken.isBlank()) {
-            authenticationListener.onSessionExpired()
-            return null
-        }
-        return runBlocking {
-            refresh(refreshToken).fold(
-                onSuccess = {
-                    tokenManager.saveAccessToken(it.accessToken)
-                    tokenManager.saveRefreshToken(it.refreshToken)
-                    AuthorizationInterceptor.from(response.request, it.accessToken)
-                },
-                onFailure = {
-                    authenticationListener.onSessionExpired()
-                    null
-                },
-            )
-        }
+        private suspend fun refresh(refreshToken: String): Result<JwtTokenResponse> =
+            runCatching {
+                val body = RefreshRequest(refreshToken = refreshToken)
+                authService.postRefresh(body = body).data
+            }
     }
-
-    private suspend fun refresh(refreshToken: String): Result<JwtTokenResponse> =
-        runCatching {
-            val body = RefreshRequest(refreshToken = refreshToken)
-            authService.postRefresh(body = body).data
-        }
-}
