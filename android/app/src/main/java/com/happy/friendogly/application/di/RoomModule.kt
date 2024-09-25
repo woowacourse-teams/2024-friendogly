b21afab9bcbce892119ec5dc5e5f673f0e0bc990
gaeun5744 <geun5744@gmail.com>
2024-09-25 17:17:31 +0900

    feat: Room 모듈 구현

---
+++ android/app/src/main/java/com/happy/friendogly/application/di/RoomModule.kt
@@ -0,0 +1,39 @@
+package com.happy.friendogly.application.di
+
+import android.content.Context
+import androidx.room.Room
+import com.happy.friendogly.domain.model.ChatRoom
+import com.happy.friendogly.local.room.ChatMessageDao
+import com.happy.friendogly.local.room.ChatMessageDatabase
+import com.happy.friendogly.local.room.ChatRoomDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    @Provides
+    @Singleton
+    fun provideChatRoomDatabase(
+        @ApplicationContext appContext: Context
+    ): ChatMessageDatabase = Room.databaseBuilder(
+        appContext, ChatMessageDatabase::class.java, "chat"
+    ).build()
+
+    @Provides
+    @Singleton
+    fun provideChatRoomDao(
+        chatMessageDatabase: ChatMessageDatabase
+    ): ChatRoomDao = chatMessageDatabase.chatRoomDao()
+
+    @Provides
+    @Singleton
+    fun provideChatMessageDao(
+        chatMessageDatabase: ChatMessageDatabase
+    ): ChatMessageDao = chatMessageDatabase.chatMessageDao()
+}
