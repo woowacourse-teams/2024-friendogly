707474dfc392ede4fe059ad8d881ba9a69dda1ec
김진우 <jinwoo3661@naver.com>
2024-09-30 12:25:43 +0900

    feat: 에러를 처리하는 ClubErrorHandler 생성

---
+++ android/app/src/main/java/com/happy/friendogly/presentation/ui/club/common/ClubErrorHandler.kt
@@ -0,0 +1,40 @@
+package com.happy.friendogly.presentation.ui.club.common
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.happy.friendogly.domain.error.DataError
+import com.happy.friendogly.presentation.base.Event
+import com.happy.friendogly.presentation.base.emit
+
+sealed interface ClubErrorEvent {
+    data object InternetError : ClubErrorEvent
+    data object FileSizeError : ClubErrorEvent
+    data object ServerError : ClubErrorEvent
+    data object UnKnownError : ClubErrorEvent
+}
+
+class ClubErrorHandler {
+    private val _error: MutableLiveData<Event<ClubErrorEvent>> = MutableLiveData(null)
+    val error: LiveData<Event<ClubErrorEvent>> get() = _error
+
+    fun handle(error: DataError) {
+        _error.emit(
+            when (error) {
+                is DataError.Network -> {
+                    when (error) {
+                        DataError.Network.NO_INTERNET -> ClubErrorEvent.InternetError
+                        DataError.Network.FILE_SIZE_EXCEED -> ClubErrorEvent.FileSizeError
+                        DataError.Network.UNKNOWN -> ClubErrorEvent.UnKnownError
+                        else -> ClubErrorEvent.ServerError
+                    }
+                }
+
+                is DataError.Local -> {
+                    //TODO : Local Exception
+                    ClubErrorEvent.UnKnownError
+                }
+            }
+        )
+    }
+}
