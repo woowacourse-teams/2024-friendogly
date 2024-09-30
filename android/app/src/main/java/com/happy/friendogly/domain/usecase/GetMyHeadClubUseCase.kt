9ef6a165269565c2244a520815343ccb469fb91b
김진우 <jinwoo3661@naver.com>
2024-09-30 12:25:23 +0900

    refactor: Usecase에 DomainResult 적용

--- android/app/src/main/java/com/happy/friendogly/domain/usecase/GetMyHeadClubUseCase.kt
+++ android/app/src/main/java/com/happy/friendogly/domain/usecase/GetMyHeadClubUseCase.kt
@@ -1,5 +1,7 @@
 package com.happy.friendogly.domain.usecase
 
+import com.happy.friendogly.domain.DomainResult
+import com.happy.friendogly.domain.error.DataError
 import com.happy.friendogly.domain.model.Club
 import com.happy.friendogly.domain.repository.MyClubRepository
 import javax.inject.Inject
@@ -9,5 +11,5 @@ class GetMyHeadClubUseCase
     constructor(
         private val repository: MyClubRepository,
     ) {
-        suspend operator fun invoke(): Result<List<Club>> = repository.getMyHeadClubs()
+        suspend operator fun invoke(): DomainResult<List<Club>,DataError.Network> = repository.getMyHeadClubs()
     }
--- android/app/src/main/java/com/happy/friendogly/domain/usecase/GetSearchingClubsUseCase.kt
+++ android/app/src/main/java/com/happy/friendogly/domain/usecase/GetSearchingClubsUseCase.kt
@@ -1,5 +1,7 @@
 package com.happy.friendogly.domain.usecase
 
+import com.happy.friendogly.domain.DomainResult
+import com.happy.friendogly.domain.error.DataError
 import com.happy.friendogly.domain.model.Club
 import com.happy.friendogly.domain.model.ClubAddress
 import com.happy.friendogly.domain.model.ClubFilterCondition
@@ -18,7 +20,7 @@ class GetSearchingClubsUseCase
             address: ClubAddress,
             genderParams: List<Gender>,
             sizeParams: List<SizeType>,
-        ): Result<List<Club>> =
+        ): DomainResult<List<Club>,DataError.Network> =
             repository.getSearchingClubs(
                 filterCondition = filterCondition,
                 address = address,
