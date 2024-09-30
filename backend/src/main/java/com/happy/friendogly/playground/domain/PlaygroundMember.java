bc7bdceb166ae5ca2e5b66866c0a4d1ad0019f94
ehtjsv2 <ehtjsv2@naver.com>
2024-09-30 14:15:56 +0900

    feat: Playground GeneratedValue 전략 설정

--- backend/src/main/java/com/happy/friendogly/playground/domain/PlaygroundMember.java
+++ backend/src/main/java/com/happy/friendogly/playground/domain/PlaygroundMember.java
@@ -4,6 +4,8 @@ import com.happy.friendogly.member.domain.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 public class PlaygroundMember {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
