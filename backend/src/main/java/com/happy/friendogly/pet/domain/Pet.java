8931aefbe44a65b78fc4f6f65a9ff32cda28b374
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-02 15:42:11 +0900

    refactor: fetch type Lazy로 변경

--- backend/src/main/java/com/happy/friendogly/pet/domain/Pet.java
+++ backend/src/main/java/com/happy/friendogly/pet/domain/Pet.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,7 +28,7 @@ public class Pet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
     private Member member;
 
