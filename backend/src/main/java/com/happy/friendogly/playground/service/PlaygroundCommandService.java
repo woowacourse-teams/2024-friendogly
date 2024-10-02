5a589b552cf99f47263b8ad621a6435ccac65d45
ehtjsv2 <ehtjsv2@naver.com>
2024-10-02 11:05:53 +0900

    refactor: commandService에 트랜잭션 추가

--- backend/src/main/java/com/happy/friendogly/playground/service/PlaygroundCommandService.java
+++ backend/src/main/java/com/happy/friendogly/playground/service/PlaygroundCommandService.java
@@ -2,8 +2,10 @@ package com.happy.friendogly.playground.service;
 
 import com.happy.friendogly.playground.repository.PlaygroundRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class PlaygroundCommandService {
 
     private final PlaygroundRepository playgroundRepository;
