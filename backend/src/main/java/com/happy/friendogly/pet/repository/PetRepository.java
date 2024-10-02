76e5cd1817b22520e42e7c85c732cb5631f27a16
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-02 15:55:42 +0900

    refactor: fetch join 추가, 메서드 순서 변경

--- backend/src/main/java/com/happy/friendogly/pet/repository/PetRepository.java
+++ backend/src/main/java/com/happy/friendogly/pet/repository/PetRepository.java
@@ -4,19 +4,35 @@ import com.happy.friendogly.exception.FriendoglyException;
 import com.happy.friendogly.member.domain.Member;
 import com.happy.friendogly.pet.domain.Pet;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
-    Long countByMember(Member member);
-
-    List<Pet> findByMemberId(Long memberId);
-
-    boolean existsByMemberId(Long memberId);
-
-    void deleteAllByMemberId(Long memberId);
+    @Query("""
+            SELECT p
+            FROM Pet p
+            JOIN FETCH p.member m
+            WHERE p.id = :id
+            """)
+    Optional<Pet> findById(Long id);
 
     default Pet getById(Long id) {
         return findById(id).orElseThrow(() -> new FriendoglyException("강아지 정보를 찾지 못했습니다."));
     }
+
+    @Query("""
+            SELECT p
+            FROM Pet p
+            JOIN FETCH p.member m
+            WHERE m.id = :memberId
+            """)
+    List<Pet> findByMemberId(Long memberId);
+
+    Long countByMember(Member member);
+
+    boolean existsByMemberId(Long memberId);
+
+    void deleteAllByMemberId(Long memberId);
 }
