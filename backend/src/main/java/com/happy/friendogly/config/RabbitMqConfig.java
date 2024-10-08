12346287d311605a8d2d3126114e22499f3a0583
takoyakimchi <c4nyou@gmail.com>
2024-10-08 21:31:35 +0900

    remove comments

--- backend/src/main/java/com/happy/friendogly/config/RabbitMqConfig.java
+++ backend/src/main/java/com/happy/friendogly/config/RabbitMqConfig.java
@@ -64,11 +64,6 @@ public class RabbitMqConfig {
                 .with(ROUTING_KEY);
     }
 
-    /**
-     * RabbitMQ 연결을 위한 ConnectionFactory 빈을 생성하여 반환
-     *
-     * @return ConnectionFactory 객체
-     */
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
@@ -87,12 +82,6 @@ public class RabbitMqConfig {
         return rabbitAdmin;
     }
 
-    /**
-     * RabbitTemplate을 생성하여 반환
-     *
-     * @param connectionFactory RabbitMQ와의 연결을 위한 ConnectionFactory 객체
-     * @return RabbitTemplate 객체
-     */
     @Bean
     public RabbitTemplate rabbitTemplate(
             org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory) {
@@ -102,11 +91,6 @@ public class RabbitMqConfig {
         return rabbitTemplate;
     }
 
-    /**
-     * Jackson 라이브러리를 사용하여 메시지를 JSON 형식으로 변환하는 MessageConverter 빈을 생성
-     *
-     * @return MessageConverter 객체
-     */
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         ObjectMapper objectMapper = new ObjectMapper();
