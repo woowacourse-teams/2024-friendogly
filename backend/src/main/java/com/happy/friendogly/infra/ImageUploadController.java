27ae87861a422afdcdc0145ca28bf81e6bbebeb2
takoyakimchi <c4nyou@gmail.com>
2024-10-11 13:05:44 +0900

    feat: implement test controller

---
+++ backend/src/main/java/com/happy/friendogly/infra/ImageUploadController.java
@@ -0,0 +1,20 @@
+package com.happy.friendogly.infra;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@Profile("dev")
+public class ImageUploadController {
+
+    private final FileStorageManager fileStorageManager;
+
+    @PostMapping("/upload")
+    public void uploadImage(MultipartFile imageFile) {
+        fileStorageManager.uploadFile(imageFile);
+    }
+}
