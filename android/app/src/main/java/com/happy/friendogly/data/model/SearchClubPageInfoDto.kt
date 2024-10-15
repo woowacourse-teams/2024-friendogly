697378cae5cb7c400360a50b1ac5f53516dede74
김진우 <jinwoo3661@naver.com>
2024-10-16 00:07:51 +0900

    feat: SearchClubPageInfo 구현

---
+++ android/app/src/main/java/com/happy/friendogly/data/model/SearchClubPageInfoDto.kt
@@ -0,0 +1,7 @@
+package com.happy.friendogly.data.model
+
+data class SearchClubPageInfoDto(
+    val pageSize: Int,
+    val lastFoundCreatedAt: String,
+    val lastFoundId: String,
+)
---
+++ android/app/src/main/java/com/happy/friendogly/domain/model/SearchClubPageInfo.kt
@@ -0,0 +1,16 @@
+package com.happy.friendogly.domain.model
+
+import kotlinx.datetime.LocalDateTime
+
+
+data class SearchClubPageInfo(
+    val pageSize: Int = DEFAULT_PAGE_SIZE,
+    val lastFoundCreatedAt: LocalDateTime = LocalDateTime.parse(DEFAULT_FOUND_CREATE),
+    val lastFoundId: Long = DEFAULT_FOUND_ID
+) {
+    companion object {
+        private const val DEFAULT_PAGE_SIZE = 20
+        private const val DEFAULT_FOUND_CREATE = "9999-12-31T23:59:59"
+        private const val DEFAULT_FOUND_ID = 0x7fffffffffffffff
+    }
+}
