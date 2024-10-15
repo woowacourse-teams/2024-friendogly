f4bb93c1031c2c2835dcdbd0c1a94f16223e464b
김진우 <jinwoo3661@naver.com>
2024-10-16 00:07:24 +0900

    feat: MyClub 분리

---
+++ android/app/src/main/java/com/happy/friendogly/presentation/ui/club/my/adapter/MyClubListAdapter.kt
@@ -0,0 +1,45 @@
+package com.happy.friendogly.presentation.ui.club.my.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.happy.friendogly.databinding.ItemClubBinding
+import com.happy.friendogly.presentation.ui.club.common.ClubItemActionHandler
+import com.happy.friendogly.presentation.ui.club.list.adapter.club.ClubViewHolder
+import com.happy.friendogly.presentation.ui.club.common.model.ClubItemUiModel
+
+class MyClubListAdapter(
+    private val actionHandler: ClubItemActionHandler,
+) : ListAdapter<ClubItemUiModel, ClubViewHolder>(ClubDiffCallback()) {
+    class ClubDiffCallback : DiffUtil.ItemCallback<ClubItemUiModel>() {
+        override fun areItemsTheSame(
+            oldItem: ClubItemUiModel,
+            newItem: ClubItemUiModel,
+        ): Boolean {
+            return oldItem.clubId == newItem.clubId
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ClubItemUiModel,
+            newItem: ClubItemUiModel,
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): ClubViewHolder {
+        val binding = ItemClubBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ClubViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: ClubViewHolder,
+        position: Int,
+    ) {
+        holder.bind(getItem(position), actionHandler)
+    }
+}
