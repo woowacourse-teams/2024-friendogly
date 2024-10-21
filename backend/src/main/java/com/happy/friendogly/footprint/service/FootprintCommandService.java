df09b9f7ff21c7136d7079da70c2d0fa348355c6
김도선 <ehtjsv2@naver.com>
2024-10-21 21:20:09 +0900

    chore: 배포를 위한 개행추가

--- backend/src/main/java/com/happy/friendogly/footprint/service/FootprintCommandService.java
+++ backend/src/main/java/com/happy/friendogly/footprint/service/FootprintCommandService.java
@@ -23,6 +23,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Service
 @Transactional
 public class FootprintCommandService {
