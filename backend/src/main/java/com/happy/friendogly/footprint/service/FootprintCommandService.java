f5badddaa085dcb3c74f41049138e4906a92fe74
김도선 <ehtjsv2@naver.com>
2024-10-21 21:45:47 +0900

    chore: 개행제거

--- backend/src/main/java/com/happy/friendogly/footprint/service/FootprintCommandService.java
+++ backend/src/main/java/com/happy/friendogly/footprint/service/FootprintCommandService.java
@@ -23,7 +23,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 @Transactional
 public class FootprintCommandService {
