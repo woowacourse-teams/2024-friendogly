250c58cd2fc8d9049d782cc1676669d1754d220f
ehtjsv2 <ehtjsv2@naver.com>
2024-12-02 14:49:39 +0900

    refactor: s3 key prefix 제거

--- backend/src/main/java/com/happy/friendogly/infra/S3StorageManager.java
+++ backend/src/main/java/com/happy/friendogly/infra/S3StorageManager.java
@@ -38,9 +38,6 @@ public class S3StorageManager implements FileStorageManager {
     @Value("${aws.s3.server.endpoint}")
     private String S3_ENDPOINT;
 
-    @Value("${aws.s3.server.key-prefix}")
-    private String KEY_PREFIX;
-
     private final S3Client s3Client;
 
     public S3StorageManager() {
@@ -64,7 +61,7 @@ public class S3StorageManager implements FileStorageManager {
 
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(BUCKET_NAME)
-                .key(KEY_PREFIX + newFilename)
+                .key(newFilename)
                 .contentType(contentType)
                 .build();
 
