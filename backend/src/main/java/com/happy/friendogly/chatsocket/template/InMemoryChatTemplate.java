aee029abbac63f95843d00b1928df0fdd4d310e8
takoyakimchi <c4nyou@gmail.com>
2024-12-03 15:40:23 +0900

    refactor: `@Profile` 명시 제거

--- backend/src/main/java/com/happy/friendogly/chatsocket/template/InMemoryChatTemplate.java
+++ backend/src/main/java/com/happy/friendogly/chatsocket/template/InMemoryChatTemplate.java
@@ -1,11 +1,9 @@
 package com.happy.friendogly.chatsocket.template;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("local | dev | prod")
 public class InMemoryChatTemplate implements ChatTemplate {
 
     private static final String TOPIC_CHAT_PREFIX = "/exchange/chat.exchange/room.";
