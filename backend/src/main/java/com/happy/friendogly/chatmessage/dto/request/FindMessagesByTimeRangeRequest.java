d83d1c0f57bd6bed894e3bb205c82f6464cf8ea9
TRE <37261785+takoyakimchi@users.noreply.github.com>
2025-01-04 15:17:20 +0900

    [BE] fix: 채팅 메시지 조회 검증 로직 제거 (#782)

--- backend/src/main/java/com/happy/friendogly/chatmessage/dto/request/FindMessagesByTimeRangeRequest.java
+++ backend/src/main/java/com/happy/friendogly/chatmessage/dto/request/FindMessagesByTimeRangeRequest.java
@@ -4,10 +4,10 @@ import jakarta.validation.constraints.Past;
 import java.time.LocalDateTime;
 
 public record FindMessagesByTimeRangeRequest(
-        @Past(message = "채팅 메시지 조회 since 시간은 과거 시간만 입력 가능합니다.")
+//        @Past(message = "채팅 메시지 조회 since 시간은 과거 시간만 입력 가능합니다.")
         LocalDateTime since,
 
-        @Past(message = "채팅 메시지 조회 until 시간은 과거 시간만 입력 가능합니다.")
+//        @Past(message = "채팅 메시지 조회 until 시간은 과거 시간만 입력 가능합니다.")
         LocalDateTime until
 ) {
 
