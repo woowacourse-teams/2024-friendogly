efa4018391f1c9e2215f8db6e79ef0733003785f
J-I-H-O <jeongjiho0731@gmail.com>
2025-04-10 12:59:34 +0900

    test: 실제 MDC 메서드를 호출하는지 검증하도록 변경

--- backend/src/test/java/com/happy/friendogly/utils/MdcLoggingTest.java
+++ backend/src/test/java/com/happy/friendogly/utils/MdcLoggingTest.java
@@ -1,40 +1,38 @@
 package com.happy.friendogly.utils;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.willReturn;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.willAnswer;
+import static org.mockito.Mockito.mockStatic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.happy.friendogly.auth.AuthArgumentResolver;
-import com.happy.friendogly.playground.dto.request.SavePlaygroundRequest;
-import com.happy.friendogly.utils.logging.MdcLoggingInterceptor;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InOrder;
+import org.mockito.MockedStatic;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MdcLoggingTest {
 
+    private static final String PUT_URI = "put-uri";
+    private static final String PUT_MEMBER_ID = "put-memberId";
+    private static final String CLEAR = "clear";
+    
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private MdcLoggingInterceptor mdcLoggingInterceptor;
-
-    @MockBean
+    @SpyBean
     private AuthArgumentResolver authArgumentResolver;
 
-    // TODO: Interceptor, ArgumentResolver 동작 여부가 아닌, MDC.put(uri), MDC.put(memberId), MDC.clear() 호출 순서 검증하기
     /**
      * 1) 사용자 요청
      * 2) Interceptor MDC.put(uri)
@@ -44,40 +42,49 @@ public class MdcLoggingTest {
      */
     @DisplayName("요청 처리 중 Interceptor와 ArgumentResolver가 일련의 순서대로 동작한다.")
     @Test
-    void mdcInOrder() throws Exception {
-        // given
-        willReturn(true).given(mdcLoggingInterceptor).preHandle(any(), any(), any());
-        willReturn(true).given(authArgumentResolver).supportsParameter(any());
-        willReturn(1L).given(authArgumentResolver).resolveArgument(any(), any(), any(), any());
+    void mdcClearStatic() throws Exception {
+        try (MockedStatic<MDC> mdcMock = mockStatic(MDC.class)) {
+            List<String> actualOrder = new ArrayList<>();
+            List<String> expectedOrder = List.of(PUT_URI, PUT_MEMBER_ID, CLEAR);
 
-        // when
-        mockMvc.perform(get("/playgrounds/locations"));
+            // given
+            mdcMock.when(() -> MDC.put("uri", "/playgrounds/locations"))
+                    .then(invocation -> actualOrder.add(PUT_URI));
 
-        // then
-        InOrder order = inOrder(mdcLoggingInterceptor, authArgumentResolver);
-        order.verify(mdcLoggingInterceptor).preHandle(any(), any(), any());
-        order.verify(authArgumentResolver).resolveArgument(any(), any(), any(), any());
-        order.verify(mdcLoggingInterceptor).afterCompletion(any(), any(), any(), any());
+            mdcMock.when(() -> MDC.put("memberId", "1"))
+                    .then(invocation -> actualOrder.add(PUT_MEMBER_ID));
+
+            mdcMock.when(() -> MDC.clear())
+                    .then(invocation -> actualOrder.add(CLEAR));
+
+            willAnswer(invocation -> {
+                MDC.put("memberId", "1");
+                return 1L;
+            }).given(authArgumentResolver).resolveArgument(any(), any(), any(), any());
+
+            // when
+            mockMvc.perform(get("/playgrounds/locations"));
+
+            // then
+            assertThat(actualOrder).containsExactlyInAnyOrderElementsOf(expectedOrder);
+        }
     }
 
     @DisplayName("요청 처리 도중 예외가 발생해도 MDC.clear() 호출이 보장된다.")
     @Test
     void mdcClear() throws Exception {
-        // given
-        willReturn(true).given(mdcLoggingInterceptor).preHandle(any(), any(), any());
-        willReturn(true).given(authArgumentResolver).supportsParameter(any());
-        willReturn(1L).given(authArgumentResolver).resolveArgument(any(), any(), any(), any());
+        try (MockedStatic<MDC> mdcMock = mockStatic(MDC.class)) {
+            List<String> actualOrder = new ArrayList<>();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        SavePlaygroundRequest invalidRequest = new SavePlaygroundRequest(999, 999);
-        String invalidContent = objectMapper.writeValueAsString(invalidRequest);
+            // given
+            mdcMock.when(() -> MDC.clear())
+                    .then(invocation -> actualOrder.add(CLEAR));
 
-        // when
-        mockMvc.perform(post("/playgrounds")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(invalidContent));
+            // when: Authorization 헤더가 없어 처리 도중 예외 발생
+            mockMvc.perform(get("/playgrounds/locations"));
 
-        // then
-        verify(mdcLoggingInterceptor).afterCompletion(any(), any(), any(), any());
+            // then
+            assertThat(actualOrder).contains(CLEAR);
+        }
     }
 }
