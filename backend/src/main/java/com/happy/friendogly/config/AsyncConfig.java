153db0ca500cb52288bbdc83a6098b7a59a932eb
ehtjsv2 <ehtjsv2@naver.com>
2025-04-30 12:21:18 +0900

    refactor: 불필요한 어노테이션 삭제

--- backend/src/main/java/com/happy/friendogly/config/AsyncConfig.java
+++ backend/src/main/java/com/happy/friendogly/config/AsyncConfig.java
@@ -4,10 +4,8 @@ import com.happy.friendogly.AsyncExceptionHandler;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
 
     @Override
